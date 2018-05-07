3ea42387459500fb4bce2f0ef2fa89b794406088
Marko Pietarinen <marko.pietarinen@fmi.fi>
2018-05-07 12:56:22 +0300

    Changed the cmake build script to work on smartmet-workstation project directory (was SmartMet directory). Output directory is now named  smartmet-workstation_msvc. Also using variable to handle directory names.

--- tools/cmake_build_script/cmake_msvc_build.sh
+++ tools/cmake_build_script/cmake_msvc_build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cd ../../.. && mkdir -p smartmet_msvc && cd smartmet_msvc && cmake -G "Visual Studio 14 2015 Win64" ../SmartMet
+BaseProjectDir="smartmet-workstation"
+
+cd ../../.. && mkdir -p ${BaseProjectDir}_msvc && cd ${BaseProjectDir}_msvc && cmake -G "Visual Studio 14 2015 Win64" ../$BaseProjectDir
 
 read -p "Press [Enter] key to continue..."
