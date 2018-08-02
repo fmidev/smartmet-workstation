f3413355b35b9c6458f4ae03324e5e294507e95f
Marko Pietarinen <marko.pietarinen@fmi.fi>
2018-08-02 09:23:46 +0300

    In CFmiExtraMapView::OnDraw method no more need to do CFmiWin32Helpers::IsWindowMinimized check to prevent problems related to wrongly handled minimized views in viewMacros.

--- src/toolboxdep/FmiExtraMapView.cpp
+++ src/toolboxdep/FmiExtraMapView.cpp
@@ -122,8 +122,6 @@ void CFmiExtraMapView::OnDraw(CDC* pDC)
 {
 	if(itsSmartMetDocumentInterface->Printing())
 		return ; // tulee ongelmia, jos ruutua päivitetään kun samalla printataan
-    if(CFmiWin32Helpers::IsWindowMinimized(this))
-        return; // tulee ongelmia, jos ruutua päivitetään kun se on minimoitu
 
 	CFmiWin32Helpers::SetDescTopGraphicalInfo(GetGraphicalInfo(), pDC, PrintViewSizeInPixels(), itsSmartMetDocumentInterface->DrawObjectScaleFactor());
 
