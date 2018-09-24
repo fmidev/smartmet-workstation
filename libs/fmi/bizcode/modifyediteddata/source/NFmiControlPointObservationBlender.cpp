0f26bfc4059361dd25508a32119c15017e8d7068
Marko Pietarinen <marko.pietarinen@fmi.fi>
2018-09-24 16:01:18 +0300

    This is first 'working' version of NFmiControlPointObservationBlender class.  It seemed to do correct looking calculations for simple Metar data case.

--- libs/fmi/bizcode/modifyediteddata/source/NFmiControlPointObservationBlender.cpp
+++ libs/fmi/bizcode/modifyediteddata/source/NFmiControlPointObservationBlender.cpp
@@ -1,6 +1,8 @@
 #include "NFmiControlPointObservationBlender.h"
 #include "NFmiEditorControlPointManager.h"
 #include "NFmiFastQueryInfo.h"
+#include "NFmiAreaMaskList.h"
+#include "NFmiDrawParam.h"
 
 NFmiControlPointObservationBlender::NFmiControlPointObservationBlender(boost::shared_ptr<NFmiFastQueryInfo> &theInfo, boost::shared_ptr<NFmiDrawParam> &theDrawParam, boost::shared_ptr<NFmiAreaMaskList> &theMaskList,
     unsigned long theAreaMask, boost::shared_ptr<NFmiEditorControlPointManager> &theCPManager, const NFmiRect &theCPGridCropRect,
@@ -21,7 +23,7 @@ static NFmiTimeDescriptor CalcAllowedTimes(const NFmiMetTime &actualFirstTime, l
     return NFmiTimeDescriptor(actualFirstTime, NFmiTimeBag(allowedRangeStartTime, actualFirstTime, expirationTimeInMinutes));
 }
 
-void NFmiControlPointObservationBlender::DoTimeSerialModifications(NFmiTimeDescriptor& theActiveTimes, NFmiThreadCallBacks *theThreadCallBacks)
+bool NFmiControlPointObservationBlender::ModifyTimeSeriesDataUsingMaskFactors(NFmiTimeDescriptor& theActiveTimes, NFmiThreadCallBacks *theThreadCallBacks)
 {
     // Kuinka vanhoja havaintoja sallitaan mukaan suhteessa aloitusaikaan itsActualFirstTime
     const long expirationTimeInMinutes = 20;
@@ -43,10 +45,65 @@ void NFmiControlPointObservationBlender::DoTimeSerialModifications(NFmiTimeDescr
             // 5. Laske CP-pisteiden avulla 0-hetken 'analyysikenttä'
             NFmiDataParamControlPointModifier::DoDataGridding(xValues, yValues, zValues, static_cast<int>(xValues.size()), itsGridData, itsGridCropRelativeRect, griddingFunction, itsObsDataGridding, kFloatMissing);
             // 6. Laske analyysikentän ja editoidun datan 0-hetken kentän avulla muutoskenttä
+            auto changeField = CalcChangeField(itsGridData);
             // 7. Blendaa muutoskenttä editoituun dataan liu'uttamalla
+            return MakeBlendingOperation(changeField, theActiveTimes);
             // 8. Lasketaanko 'analyysit' ja niiden sijoitus editoituun dataan myös 0-hetkeä edeltäville editoidun datan ajoille?
         }
     }
+    return false;
+}
+
+// Blendataan annettu muutoskenttä editoituun parametriin annetun blendingTimes:in aikarajoissa.
+bool NFmiControlPointObservationBlender::MakeBlendingOperation(const NFmiDataMatrix<float> &changeField, NFmiTimeDescriptor &blendingTimes)
+{
+    auto status = false;
+    auto &editedInfo = itsInfo;
+    NFmiDataParamModifier::LimitChecker limitChecker(static_cast<float>(itsDrawParam->AbsoluteMinValue()), static_cast<float>(itsDrawParam->AbsoluteMaxValue()), static_cast<FmiParameterName>(editedInfo->Param().GetParamIdent()));
+    for(editedInfo->ResetLevel(); editedInfo->NextLevel(); ) // Tuskin ikinä päästään 3D editointiin, mutta varaudutaan silti siihen
+    {
+        auto blendingTimeSize = blendingTimes.Size();
+        for(blendingTimes.Reset(); blendingTimes.Next(); )
+        {
+            auto blendingTimeIndex = blendingTimes.Index();
+            // Blendaus menee niin että alkuhetkellä muutos kenttä otetaan kertoimella 1. ja viimeisellä se otetaan kertoimella 0. (eli viimeinen aika-askel voidaan skipata)
+            if(blendingTimeIndex + 1 >= blendingTimes.Size())
+                break;
+            const auto &editedTime = blendingTimes.Time();
+            if(editedInfo->Time(editedTime)) // Ajan pitäisi aina löytyä, koska blendingtimes on rakennettu editoidun datan aikojen perusteella
+            {
+                itsParamMaskList->SyncronizeMaskTime(editedTime);
+                for(editedInfo->ResetLocation(); editedInfo->NextLocation(); )
+                {
+                    if(itsParamMaskList->IsMasked(editedInfo->LatLonFast()))
+                    {
+                        auto editedValue = editedInfo->FloatValue();
+                        auto changeValue = changeField.GetValue(editedInfo->LocationIndex(), kFloatMissing);
+                        float value = NFmiControlPointObservationBlender::BlendData(editedValue, changeValue, blendingTimeSize, blendingTimeIndex, limitChecker);
+                        editedInfo->FloatValue(value);
+                        status = true;
+                    }
+                }
+            }
+        }
+    }
+    return status;
+}
+
+float NFmiControlPointObservationBlender::BlendData(float editedDataValue, float changeValue, unsigned long timeSize, unsigned long timeIndex, const NFmiDataParamModifier::LimitChecker &limitChecker)
+{
+    if(editedDataValue == kFloatMissing || changeValue == kFloatMissing)
+        return editedDataValue; // ongelma tilanteissa palautetaan arvo editoidusta datasta takaisin
+    float value = (timeSize - timeIndex - 1) / (timeSize - 1) * changeValue + editedDataValue;
+    return limitChecker.CheckValue(value);
+}
+
+NFmiDataMatrix<float> NFmiControlPointObservationBlender::CalcChangeField(const NFmiDataMatrix<float> &analysisField)
+{
+    NFmiDataMatrix<float> changeField;
+    itsInfo->Values(changeField); // otetaan originaali kenttä changeField:iin
+    changeField -= analysisField; // matriisi laskuoperaatiot ottavat huomioon puuttuvat arvot
+    return changeField;
 }
 
 const double g_missingDistanceToStationInKm = 9999999999.;
@@ -65,7 +122,7 @@ void NFmiControlPointObservationBlender::FillZeroChangeValuesForMissingCpPoints(
 
 
 // Hae arvot sallituilta asemilta sallituilta ajoilta, jos ei arvoa, muutos kyseisessä CP-pisteessä on 0 (merkitään missing arvolla).
-bool NFmiControlPointObservationBlender::GetObservationsToChangeValueFields(std::vector<float> xValues, std::vector<float> yValues, std::vector<float> zValues, const NFmiTimeDescriptor &allowedTimeRange, double maxAllowedDistanceToStationInKm)
+bool NFmiControlPointObservationBlender::GetObservationsToChangeValueFields(std::vector<float> &xValues, std::vector<float> &yValues, std::vector<float> &zValues, const NFmiTimeDescriptor &allowedTimeRange, double maxAllowedDistanceToStationInKm)
 {
     // 1. Haetaan käytetystä CP-managerista pohjat, eli x- ja y-koordinaatit ja jotkut pohja-arvot z eli muutoskentälle.
     if(GetChangeValues(xValues, yValues, zValues))
