2b92d53b1a20a23c893976d6de51022cd03b9259
Marko Pietarinen <marko.pietarinen@fmi.fi>
2018-10-11 07:32:25 +0300

    Changed version's status to 'Beta' and also added couple of lines of warnings about possible problems with editing tools.

--- libs/fmi/bizcode/generaldoc/source/NFmiBasicSmartMetConfigurations.cpp
+++ libs/fmi/bizcode/generaldoc/source/NFmiBasicSmartMetConfigurations.cpp
@@ -503,13 +503,17 @@ void NFmiBasicSmartMetConfigurations::MakeSplashScreenTextDataVector(const NFmiT
     copyrightStringU_ += _TEXT(" Finnish Meteorological Institute");
     itsSplashScreenTextDataVector.push_back(DrawStringData(copyrightStringU_, _TEXT("Arial"), 18, RGB(0, 0, 0), CPoint(22, 325), true));
 
-    bool betaVersion = false;
+    bool betaVersion = true;
     if(betaVersion)
         itsSplashScreenTextDataVector.push_back(DrawStringData(_TEXT("Beta"), _TEXT("Arial"), 25, RGB(0, 0, 0), CPoint(122, 267), true));
 
-    CString possibleWarningTextU_; // = _TEXT("Wms ja Hake + KaHa sanomien tuki otettu pois päältä!");
+    CString possibleWarningTextU_ = _TEXT("Uuden 'CP obs-blending' työkalun lisäys on saattanut rikkoa esim.");
     if(possibleWarningTextU_.GetLength())
         itsSplashScreenTextDataVector.push_back(DrawStringData(possibleWarningTextU_, _TEXT("Arial"), 25, RGB(255, 0, 0), CPoint(22, 370), true));
+
+    CString possibleWarningText2U_ = _TEXT("Analyysi- ja Kontrollipiste -työkalujen toimintaa");
+    if(possibleWarningText2U_.GetLength())
+        itsSplashScreenTextDataVector.push_back(DrawStringData(possibleWarningText2U_, _TEXT("Arial"), 25, RGB(255, 0, 0), CPoint(22, 395), true));
 }
 
 // Oletus, theControlPath on absoluuttinen polku joko tiedostoon tai hakemistoon.
