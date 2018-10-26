6f2e330d6dc7259d251a24c3e513396d2aacec21
Marko Pietarinen <marko.pietarinen@fmi.fi>
2018-10-26 10:38:17 +0300

    Added NFmiGriddingProperties::toString and fromString so that I can add new gridding features to MP-CP process (have to be able to move properties ovet tcp connections).

--- modules/smarttools/smarttools/NFmiGriddingProperties.cpp
+++ modules/smarttools/smarttools/NFmiGriddingProperties.cpp
@@ -1,9 +1,49 @@
 #include "NFmiGriddingProperties.h"
 #include "NFmiArea.h"
+#include <boost/algorithm/string.hpp>
 
 NFmiGriddingProperties::NFmiGriddingProperties(bool toolMasterAvailable)
     :toolMasterAvailable_(toolMasterAvailable)
 {}
+
+// Muodostetaan yhtenäinen string esitys kaikista asetuksista (paitsi toolMasterAvailable_ asetuksesta), 
+// jossa arvot on eroteltu pilkuilla. Arvot ovat luokan esittelyjärjestyksessä. Esimerkiksi oletus arvoilla saatu tulos:
+// 1,0,1,0.5,0,1.25,0.15
+std::string NFmiGriddingProperties::toString() const
+{
+    std::ostringstream out;
+    out << function_ << "," << rangeLimitInKm_ << "," << localFitMethod_ << "," << localFitDelta_ << "," << smoothLevel_ << "," << localFitFilterRadius_ << "," << localFitFilterFactor_;
+    return out.str();
+}
+
+// Lukee arvot luokan annetusta stringistä (paitsi toolMasterAvailable_ asetusta).
+// Palauttaa true, jos luku onnistui ja asetukset otetaan käyttöön.
+// Jos luvussa tulee mitään ongelmia, säilytetään olion originaali arvot ja palautetaan false.
+bool NFmiGriddingProperties::fromString(const std::string &str)
+{
+    NFmiGriddingProperties origValues = *this;
+    try
+    {
+        std::vector<std::string> strs;
+        boost::split(strs, str, boost::is_any_of(","));
+        if(strs.size() >= 7)
+        {
+            auto strsIndex = 0;
+            function_ = static_cast<FmiGriddingFunction>(std::stoi(strs[strsIndex++]));
+            rangeLimitInKm_ = std::stod(strs[strsIndex++]);
+            localFitMethod_ = std::stoi(strs[strsIndex++]);
+            localFitDelta_ = std::stod(strs[strsIndex++]);
+            smoothLevel_ = std::stoi(strs[strsIndex++]);
+            localFitFilterRadius_ = std::stod(strs[strsIndex++]);
+            localFitFilterFactor_= std::stod(strs[strsIndex++]);
+            return true;
+        }
+    }
+    catch(...)
+    { }
+    *this = origValues;
+    return false;
+}
 
 FmiGriddingFunction NFmiGriddingProperties::function(void) const
 {
