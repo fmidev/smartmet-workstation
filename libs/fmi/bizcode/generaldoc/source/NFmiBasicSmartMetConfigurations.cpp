ee2ba10342a88e515a2fba33716b99a46907cbb7
Marko Pietarinen <marko.pietarinen@fmi.fi>
2018-12-10 15:19:23 +0200

    Changed version number to 5.12.17.2 and version status to 'official' (not beta)

--- libs/fmi/bizcode/generaldoc/source/NFmiBasicSmartMetConfigurations.cpp
+++ libs/fmi/bizcode/generaldoc/source/NFmiBasicSmartMetConfigurations.cpp
@@ -503,15 +503,15 @@ void NFmiBasicSmartMetConfigurations::MakeSplashScreenTextDataVector(const NFmiT
     copyrightStringU_ += _TEXT(" Finnish Meteorological Institute");
     itsSplashScreenTextDataVector.push_back(DrawStringData(copyrightStringU_, _TEXT("Arial"), 18, RGB(0, 0, 0), CPoint(22, 325), true));
 
-    bool betaVersion = true;
+    bool betaVersion = false;
     if(betaVersion)
         itsSplashScreenTextDataVector.push_back(DrawStringData(_TEXT("Beta"), _TEXT("Arial"), 25, RGB(0, 0, 0), CPoint(122, 267), true));
 
-    CString possibleWarningTextU_ = _TEXT("Paljon muutoksia näyttöjen 'likaus' ja päivitys rutiineissa");
+    CString possibleWarningTextU_; // = _TEXT("Paljon muutoksia näyttöjen 'likaus' ja päivitys rutiineissa");
     if(possibleWarningTextU_.GetLength())
         itsSplashScreenTextDataVector.push_back(DrawStringData(possibleWarningTextU_, _TEXT("Arial"), 25, RGB(255, 0, 0), CPoint(22, 370), true));
 
-    CString possibleWarningText2U_ = _TEXT("liittyen optimointeihin, kaikkea pitäisi testata!");
+    CString possibleWarningText2U_; // = _TEXT("liittyen optimointeihin, kaikkea pitäisi testata!");
     if(possibleWarningText2U_.GetLength())
         itsSplashScreenTextDataVector.push_back(DrawStringData(possibleWarningText2U_, _TEXT("Arial"), 25, RGB(255, 0, 0), CPoint(22, 395), true));
 }
