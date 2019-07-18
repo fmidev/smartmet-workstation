2fd84fe386027bac1c8dec62f3f8df89f3777264
Marko Pietarinen <marko.pietarinen@fmi.fi>
2019-07-18 09:39:40 +0300

    Parameter Selection dialog had this feature: If you open dialog before some data is not read to the system. And after the data is read, it would be totally opened to the last tree node level. Fixed problem in following steps: 1. Added new CollapseAllTreeNodes method 2. It's used to close all nodes before opening all the non-collapsed nodes (used to collapse just the 1st category's nodes)

--- src/dialogs/FmiParameterSelectionDlg.cpp
+++ src/dialogs/FmiParameterSelectionDlg.cpp
@@ -1046,12 +1046,27 @@ void CFmiParameterSelectionDlg::ExpandAllNodes()
     itsTreeColumn.TreeRefreshRows();
 }
 
+void CFmiParameterSelectionDlg::CollapseAllTreeNodes()
+{
+    const auto& rowItemData = itsParameterSelectionSystem->dialogRowData();
+    int currentRowCount = itsGridCtrl.GetFixedRowCount();
+    // Go through all top level (category) nodes and collapse them
+    for(const auto& rowItem : rowItemData)
+    {
+        if(rowItem.rowType() == AddParams::kCategoryType)
+        {
+            itsTreeColumn.TreeDataCollapseAllSubLevels(currentRowCount);
+        }
+        currentRowCount++;
+    }
+}
+
 void CFmiParameterSelectionDlg::MakeTreeNodeCollapseSettings()
 {
     const auto &rowItemData = itsParameterSelectionSystem->dialogRowData();
     int currentRowCount = itsGridCtrl.GetFixedRowCount();
     // First collapse all nodes
-    itsTreeColumn.TreeDataCollapseAllSubLevels(currentRowCount);
+    CollapseAllTreeNodes();
     // Then open them one by one according to settings
     for(const auto &rowItem : rowItemData)
     {
