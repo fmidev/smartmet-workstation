8c6c3b97a3510040eb3e213f90b7269e931bb28f
Marko Pietarinen <marko.pietarinen@fmi.fi>
2019-08-23 08:02:53 +0300

    Modified some file save/store related functions: 1. BetaProduct::GetFilePathFromUser   - Added new theInitialFileName -parameter   - theInitialFileName and theInitialDirectory are used to construct initial file path   - This file path is given to CFileDialog as parameter and it will open the file browser at that location   - Prior usage of initial-directory didn't work as intended but the very first time in applications run time 2. BetaProduct::SaveObjectInJsonFormat   - Added new theInitialFileName -parameter that is given to GetFilePathFromUser function   - theUsedAbsoluteFilePath parameter doesn't have default value anymore 3. BetaProduct::LoadObjectInJsonFormat   - Gives empty theInitialFileName -parameter to GetFilePathFromUser function

--- libs/fmi/bizcode/autocomplete/source/NFmiBetaProductHelperFunctions.cpp
+++ libs/fmi/bizcode/autocomplete/source/NFmiBetaProductHelperFunctions.cpp
@@ -29,10 +29,13 @@ namespace BetaProduct
         return initialSavePath;
     }
 
-    bool GetFilePathFromUser(const std::string &theFileFilter, const std::string &theInitialDirectory, std::string &theFilePathOut, bool fLoadFile)
+    // theInitialFileName on siis vain polku/xxx.ext -tyyppisessa polussa xxx -osio.
+    // Kun CFileDialog:ille annetaan alkuarvaus tiedoston nimestä täysine polkuineen, avataan dialogi aina halutussa kansiossa.
+    bool GetFilePathFromUser(const std::string &theFileFilter, const std::string &theInitialDirectory, std::string &theFilePathOut, bool fLoadFile, const std::string& theInitialFileName)
     {
-        CFileDialog dlg(fLoadFile, NULL, NULL, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, CA2T(theFileFilter.c_str()));
-        dlg.m_ofn.lpstrInitialDir = CA2T(theInitialDirectory.c_str());
+        CString initialFilePath = CA2T(theInitialDirectory.c_str());
+        initialFilePath += CA2T(theInitialFileName.c_str());
+        CFileDialog dlg(fLoadFile, NULL, initialFilePath, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, CA2T(theFileFilter.c_str()));
         if(dlg.DoModal() == IDOK)
         {
             CString loadFilePath = dlg.GetPathName();
