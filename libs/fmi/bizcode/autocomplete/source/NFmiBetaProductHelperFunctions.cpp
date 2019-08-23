1c3b1843949b457dc7743d2dde05e5674d95032c
Marko Pietarinen <marko.pietarinen@fmi.fi>
2019-08-23 13:04:12 +0300

    Added BetaProduct::GetFilePathFromUserTotal -function that will do the GetFilePathFromUser -function stuff and also forces that user gives wanted extension to different macro-files and also double checks (on save) that file doesn't exist or ask replace conformations.

--- libs/fmi/bizcode/autocomplete/source/NFmiBetaProductHelperFunctions.cpp
+++ libs/fmi/bizcode/autocomplete/source/NFmiBetaProductHelperFunctions.cpp
@@ -2,6 +2,8 @@
 #include "NFmiBetaProductHelperFunctions.h"
 #include "NFmiFileString.h"
 #include "NFmiStringTools.h"
+#include "NFmiFileSystem.h"
+#include "NFmiDictionaryFunction.h"
 #include <afxdlgs.h>
 
 namespace BetaProduct
@@ -45,4 +47,24 @@ namespace BetaProduct
         return false;
     }
 
+    bool GetFilePathFromUserTotal(const std::string& theFileFilter, const std::string& theInitialDirectory, std::string& theFilePathOut, bool fLoadFile, const std::string& theInitialFileName, const std::string& theFileExtension, const std::string& theRootDirectory, CWnd *theView)
+    {
+        if(BetaProduct::GetFilePathFromUser(theFileFilter, theInitialDirectory, theFilePathOut, fLoadFile, theInitialFileName))
+        {
+            auto originalFilePath = theFilePathOut;
+            theFilePathOut = PathUtils::getTrueFilePath(theFilePathOut, theRootDirectory, theFileExtension);
+            // Must check again if given file already exists, if user has given filename without extension
+            if(originalFilePath != theFilePathOut && NFmiFileSystem::FileExists(theFilePathOut))
+            {
+                std::string message = ::GetDictionaryString("File:\n");
+                message += theFilePathOut;
+                message += "\n" + ::GetDictionaryString("already exists, do you want to overwrite it?");
+                std::string messageBoxTitle = ::GetDictionaryString("File overwrite");
+                if(::MessageBox(theView->GetSafeHwnd(), CA2T(message.c_str()), CA2T(messageBoxTitle.c_str()), MB_OKCANCEL | MB_ICONWARNING) == IDCANCEL)
+                    return false;
+            }
+            return true;
+        }
+        return false;
+    }
 }
