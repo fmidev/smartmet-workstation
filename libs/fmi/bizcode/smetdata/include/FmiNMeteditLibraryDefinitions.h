b38dbc756584a8b167e8df7489db77f7c78eeeb4
Marko Pietarinen <marko.pietarinen@fmi.fi>
2019-09-24 16:40:27 +0300

    Lisäsin SmartMetiin "Swap view rows" -toiminnon. Kun parametri boksia klikkaa rivinumeron kohdalta hiiren oikealla, tulee popup-valikkoon uusi kohta, jossa lukee "Swap row x with" ja siitä aukeaa alivalikko, missä on kaikki ne rivit joihin klikattu rivi voidaan vaihtaa. Karttanäytöllä vaihtoehtoja on 49 ja poikkileikkausnäytössä 4. Optimoin rivinvaihdon siten että kaikki cachessa olevat jutut swapataan rivien välillä, joten tämä operaatio on nopea. Swappaus tapahtuu niin karttaruutu kohtaisille kuva cachelle että macroParam data cachelle. Optimointi on kuitenkin aina vaikeaa, joten toivottavasti tämä ei aiheuttanut ongelmia minkään suhteen (fiksuinta olisi ollut vain liata kaikki ja alkaa piirrot ja laskut alusta).

--- libs/fmi/bizcode/smetdata/include/FmiNMeteditLibraryDefinitions.h
+++ libs/fmi/bizcode/smetdata/include/FmiNMeteditLibraryDefinitions.h
@@ -112,7 +112,8 @@ typedef enum
     kFmiShowHelperData4OnTimeSerialView = 84,
     kFmiDontShowHelperData4OnTimeSerialView = 85,
     kFmiObservationStationsToCpPoints = 86,
-    kFmiToggleShowLegendState = 87
+    kFmiToggleShowLegendState = 87,
+    kFmiSwapViewRows = 88
 }FmiMenuCommandType;
 
 // s‰‰ koodi annetaan t‰nne. Jos se on AWS wawa koodia (arvo 100-199), konvertoidaan se WW-koodiksi, muuten
