fd9a4fb075d893243a56d22d64db83f495613f31
Marko Pietarinen <marko.pietarinen@fmi.fi>
2019-10-08 17:38:22 +0300

    Vietnamese locale code-table character set Windows-1258 related startup crash fix. Fixed functions fromLocaleStringToUtf8 and fromUtf8toLocaleString to handle exceptions and making error logs. Problem was caused that in one of Vietnamese PC there were following Windows settings: Region/Formats Vietnamese. This causes boost::locale::conv::to_utf<char> -function to throw "Invalid or unsupported charset:windows-1258" -exception when using locale that is retrieved with boost::locale::util::get_system_locale -function.

--- libs/fmi/bizcode/ctrlviewutils/source/Utf8ConversionFunctions.cpp
+++ libs/fmi/bizcode/ctrlviewutils/source/Utf8ConversionFunctions.cpp
@@ -1,20 +1,65 @@
 #include "Utf8ConversionFunctions.h"
+#include "catlog/catlog.h"
 #include "boost/locale.hpp"
 #include <locale>
 #include <codecvt>
 
-std::string fromLocaleStringToUtf8(const std::string &localeStr)
+namespace
 {
-    boost::locale::generator g;
-    g.locale_cache_enabled(true);
-    std::locale loc = g(boost::locale::util::get_system_locale());
-    return boost::locale::conv::to_utf<char>(localeStr,loc);
+    void makeErrorLogging(std::string parameterName, std::string functionName, const std::string& parameterValue, const std::exception* exceptionPtr = nullptr)
+    {
+        std::string errorMessage = exceptionPtr ? "Error in " : "Unknown error in ";
+        errorMessage += functionName + " function with " + parameterName + " '";
+        errorMessage += parameterValue + "'";
+        if(exceptionPtr)
+        {
+            errorMessage += " => ";
+            errorMessage += exceptionPtr->what();
+        }
+        CatLog::logMessage(errorMessage, CatLog::Severity::Error, CatLog::Category::Operational, true);
+    }
 }
 
-std::string fromUtf8toLocaleString(const std::string &utf8Str)
+std::string fromLocaleStringToUtf8(const std::string& localeStr)
 {
-    boost::locale::generator g;
-    g.locale_cache_enabled(true);
-    std::locale loc = g(boost::locale::util::get_system_locale());
-    return boost::locale::conv::from_utf<char>(utf8Str,loc);
+    try
+    {
+        boost::locale::generator g;
+        g.locale_cache_enabled(true);
+        std::locale loc = g(boost::locale::util::get_system_locale());
+        return boost::locale::conv::to_utf<char>(localeStr, loc);
+    }
+    catch(std::exception& e)
+    {
+        ::makeErrorLogging("localeStr", __FUNCTION__, localeStr, &e);
+    }
+    catch(...)
+    {
+        ::makeErrorLogging("localeStr", __FUNCTION__, localeStr);
+    }
+
+    // Virhetilanteissa palautetaan vain originaali localeStr
+    return localeStr;
+}
+
+std::string fromUtf8toLocaleString(const std::string& utf8Str)
+{
+    try
+    {
+        boost::locale::generator g;
+        g.locale_cache_enabled(true);
+        std::locale loc = g(boost::locale::util::get_system_locale());
+        return boost::locale::conv::from_utf<char>(utf8Str, loc);
+    }
+    catch(std::exception& e)
+    {
+        ::makeErrorLogging("utf8Str", __FUNCTION__, utf8Str, &e);
+    }
+    catch(...)
+    {
+        ::makeErrorLogging("utf8Str", __FUNCTION__, utf8Str);
+    }
+
+    // Virhetilanteissa palautetaan vain originaali utf8Str
+    return utf8Str;
 }
