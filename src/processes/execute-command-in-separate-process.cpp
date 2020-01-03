95ed5152e8c3e5d7fdc78790dccc47f1dc4a3ca2
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-01-03 21:01:01 +0200

    Added new Make7zipExePath (returns full path to 7-zip executable) function to CFmiProcessHelpers namespace. It's used from two places, so I made it general purpose function.

--- src/processes/execute-command-in-separate-process.cpp
+++ src/processes/execute-command-in-separate-process.cpp
@@ -4,73 +4,88 @@
 #include "execute-command-in-separate-process.h"
 #include "catlog/catlog.h"
 
-bool CFmiProcessHelpers::ExecuteCommandInSeparateProcess(std::string &theCommand, bool logEvents, bool showErrorMessageBox, WORD theShowWindow, bool waitExecutionToStop, DWORD dwCreationFlags, const std::string *theWorkingDirectory)
+namespace CFmiProcessHelpers
 {
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
 
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si);
-	si.dwFlags = STARTF_USESHOWWINDOW;
-	si.wShowWindow = theShowWindow;
-    CString usedWorkingDirectory;
-    LPCTSTR usedWorkingDirectoryPtr = NULL;
-    if(theWorkingDirectory)
+    bool ExecuteCommandInSeparateProcess(std::string& theCommand, bool logEvents, bool showErrorMessageBox, WORD theShowWindow, bool waitExecutionToStop, DWORD dwCreationFlags, const std::string* theWorkingDirectory)
     {
-        usedWorkingDirectory = CA2T(theWorkingDirectory->c_str());
-        usedWorkingDirectoryPtr = usedWorkingDirectory;
-    }
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
 
-// Start the child process.
-	if(!CreateProcess(
-		NULL, // No module name (use command line).
-		CA2T(theCommand.c_str()),
-		NULL,             // Process handle not inheritable.
-		NULL,             // Thread handle not inheritable.
-		FALSE,            // Set handle inheritance to FALSE.
-		dwCreationFlags,  // Creation flags.
-		NULL,             // Use parent's environment block.
-        usedWorkingDirectoryPtr,  // NULL = use parent's starting directory.
-		&si,              // Pointer to STARTUPINFO structure.
-		&pi )             // Pointer to PROCESS_INFORMATION structure.
-		)
-	{
-        LPTSTR lpMsgBuf=0;
-		FormatMessage
-		(
-			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-			NULL,
-			GetLastError(),
-			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
-			lpMsgBuf,
-			0,
-			NULL
-		);
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        si.dwFlags = STARTF_USESHOWWINDOW;
+        si.wShowWindow = theShowWindow;
+        CString usedWorkingDirectory;
+        LPCTSTR usedWorkingDirectoryPtr = NULL;
+        if(theWorkingDirectory)
+        {
+            usedWorkingDirectory = CA2T(theWorkingDirectory->c_str());
+            usedWorkingDirectoryPtr = usedWorkingDirectory;
+        }
 
-        if(showErrorMessageBox)
-    		MessageBox( NULL, lpMsgBuf, _TEXT("Error with CreateProcess call"), MB_OK|MB_ICONWARNING );
+        // Start the child process.
+        if(!CreateProcess(
+            NULL, // No module name (use command line).
+            CA2T(theCommand.c_str()),
+            NULL,             // Process handle not inheritable.
+            NULL,             // Thread handle not inheritable.
+            FALSE,            // Set handle inheritance to FALSE.
+            dwCreationFlags,  // Creation flags.
+            NULL,             // Use parent's environment block.
+            usedWorkingDirectoryPtr,  // NULL = use parent's starting directory.
+            &si,              // Pointer to STARTUPINFO structure.
+            &pi)             // Pointer to PROCESS_INFORMATION structure.
+            )
+        {
+            LPTSTR lpMsgBuf = 0;
+            FormatMessage
+            (
+                FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                NULL,
+                GetLastError(),
+                MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+                lpMsgBuf,
+                0,
+                NULL
+            );
 
-		LocalFree( lpMsgBuf );
+            if(showErrorMessageBox)
+                MessageBox(NULL, lpMsgBuf, _TEXT("Error with CreateProcess call"), MB_OK | MB_ICONWARNING);
+
+            LocalFree(lpMsgBuf);
+            if(logEvents)
+            {
+                std::string virheStr("ExecuteCommandInSeparateProcess: Command '");
+                virheStr += theCommand;
+                virheStr += "' was unable to execute.";
+                CatLog::logMessage(virheStr, CatLog::Severity::Error, CatLog::Category::Operational);
+            }
+            return false;
+        }
+        DWORD waitTimeInMS = waitExecutionToStop ? INFINITE : 0; // joko odotetaan että prosessi saadaan päätökseen, tai ei odoteta ollenkaan
+        WaitForSingleObject(pi.hProcess, waitTimeInMS);
+
+        CloseHandle(pi.hProcess); // Close process and thread handles.
+        CloseHandle(pi.hThread);
         if(logEvents)
         {
-            std::string virheStr("ExecuteCommandInSeparateProcess: Command '");
-            virheStr += theCommand;
-            virheStr += "' was unable to execute.";
-            CatLog::logMessage(virheStr, CatLog::Severity::Error, CatLog::Category::Operational);
+            std::string logStr("ExecuteCommandInSeparateProcess: Command '");
+            logStr += theCommand;
+            logStr += "' executed.";
+            CatLog::logMessage(logStr, CatLog::Severity::Info, CatLog::Category::Operational);
         }
-		return false;
-	}
-	DWORD waitTimeInMS = waitExecutionToStop ? INFINITE : 0; // joko odotetaan että prosessi saadaan päätökseen, tai ei odoteta ollenkaan
-	WaitForSingleObject( pi.hProcess, waitTimeInMS);
-
-	CloseHandle( pi.hProcess ); // Close process and thread handles.
-	CloseHandle( pi.hThread );
-    if(logEvents)
-    {
-        std::string logStr("ExecuteCommandInSeparateProcess: Command '");
-        logStr += theCommand;
-        logStr += "' executed.";
-        CatLog::logMessage(logStr, CatLog::Severity::Info, CatLog::Category::Operational);
+        return true;
     }
-    return true;
-}
+
+    std::string Make7zipExePath(const std::string& workingDirectory)
+    {
+        std::string zipCommandStr = "\""; // myös zip-commandin ympärille lainausmerkit
+        zipCommandStr += workingDirectory;
+        zipCommandStr += "\\utils\\";
+        zipCommandStr += "7z.exe";
+        zipCommandStr += "\"";
+        return zipCommandStr;
+    }
+
+} // CFmiProcessHelpers namespace
