fca0a7c6edff9332f7b07cb5a53ad92457d7d59a
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-01-03 10:57:45 +0200

    Done some cleaning with stdafx.h files all over: 1. #include <stdint.h> -stuff is now obsolete (was concerning only VC++ 2010 version) 2. Somewhere removed MFC support for Windows Common Controls includes 3. In fewer places could removed unnecessary #include <afxext.h> 4. Removed some includes for following stuff in some places:  - MFC Automation classes  - MFC support for Internet Explorer 4 Common Controls  - MFC support for Windows Common Controls  - MFC support for ribbons and control bars  - MFC socket extensions

--- src/casestudyexe/stdafx.h
+++ src/casestudyexe/stdafx.h
@@ -5,13 +5,6 @@
 
 #pragma once
 
-// T‰m‰ stdint vs intsafe -viritys est‰‰ lukemattomien Visual C++ 2010 header vikojen aiheuttamia varoituksia,
-// joka on tarkoitus korjata tulevissa p‰ivityksiss‰.
-#if _MSC_VER == 1600 // T‰m‰ korjaus tarvitaan vain Visual C++ 2010 k‰‰nt‰j‰ss‰ (= 1600, SP1 ei korjannut onhelmaa), vanhemmissa versioissa ei ole koko stdint.h -headeria
-#define _INTSAFE_H_INCLUDED_
-#include <stdint.h>
-#endif
-
 #ifndef _SECURE_ATL
 #define _SECURE_ATL 1
 #endif
@@ -30,26 +23,5 @@
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
 
-
-
-
-
-#ifndef _AFX_NO_OLE_SUPPORT
-#include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
-#endif
-#ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>             // MFC support for Windows Common Controls
-#endif // _AFX_NO_AFXCMN_SUPPORT
-
 #include <afxcontrolbars.h>     // MFC support for ribbons and control bars
-
-
-
-
-
-
-
-
-
-
 
