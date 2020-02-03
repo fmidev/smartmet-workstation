aafde710755ace958341b154457a935c7888a9f4
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-02-03 08:47:04 +0200

    Added IsolineDataDownSizingNeeded function to the public interface. Function was originally local in the cpp file and was named DataDownSizingNeeded.

--- libs/fmi/bizcode/tm_utils/include/ToolMasterDrawingFunctions.h
+++ libs/fmi/bizcode/tm_utils/include/ToolMasterDrawingFunctions.h
@@ -3,10 +3,12 @@
 #pragma once
 
 #include <string>
+#include "boost/shared_ptr.hpp"
 
 class NFmiIsoLineData;
 class NFmiRect;
 class NFmiPoint;
+class NFmiDrawParam;
 
 const int s_rgbDefCount = 4;
 
@@ -15,6 +17,7 @@ const int s_rgbDefCount = 4;
 int ToolMasterDraw(CDC* pDC, NFmiIsoLineData* theIsoLineData, const NFmiRect& theRelViewRect, const NFmiRect& theZoomedViewRect, const NFmiPoint &theGrid2PixelRatio, int theCrossSectionIsoLineDrawIndex);
 void CreateClassesAndColorTableAndColorShade(float min, float max, int classCount, int defaultTableColorIndices[s_rgbDefCount], int shadingScaleIndex, int colorTableIndex, float colorWidths[s_rgbDefCount], bool fDoIsolines);
 float CalcMMSizeFactor(float theViewHeightInMM, float theMaxFactor);
+bool IsolineDataDownSizingNeeded(const NFmiIsoLineData& theOrigIsoLineData, const NFmiPoint& theGrid2PixelRatio, NFmiPoint& theDownSizeFactorOut, const boost::shared_ptr<NFmiDrawParam> &thePossibleDrawParam = nullptr);
 #else
 // Here are dymmy versions of functions with dummy definitions, if Uniras ToolMaster isn't supported (must be inline because VC++ linker...)
 inline int ToolMasterDraw(CDC* pDC, NFmiIsoLineData* theIsoLineData, const NFmiRect& theRelViewRect, const NFmiRect& theZoomedViewRect, const NFmiPoint &theGrid2PixelRatio, int theCrossSectionIsoLineDrawIndex)
