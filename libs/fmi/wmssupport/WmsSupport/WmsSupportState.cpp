1a9d68cd3d361ee02eeebd89b632fd45e05349a1
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-03-17 18:56:52 +0200

    Moved method inplementations to the cpp file. Actually both header and cpp had the same context and now code is correctly divided. Added couple new methods: getBackgroundsLength and getOverlaysLenght

--- libs/fmi/wmssupport/WmsSupport/WmsSupportState.cpp
+++ libs/fmi/wmssupport/WmsSupport/WmsSupportState.cpp
@@ -1,65 +1,55 @@
-#pragma once
-#include "WmsSupport/Setup.h"
-
-#include <map>
+#include "WmsSupport/WmsSupportState.h"
 
 namespace Wms
 {
-    class WmsSupportState
+    WmsSupportState::WmsSupportState(const Setup& setup)
     {
-        int layerGroupIndex_;
-        int overlayIndex_;
-        int backgroundsLength_;
-        int overlaysLenght_;
-    public:
-        WmsSupportState(const Setup& setup)
+        backgroundsLength_ = static_cast<int>(setup.background.parsedServers.size());
+        overlaysLenght_ = static_cast<int>(setup.overlay.parsedServers.size());
+        layerGroupIndex_ = 0;
+        overlayIndex_ = -1;
+    }
+
+    int WmsSupportState::getCurrentBackgroundIndex() const
+    {
+        return layerGroupIndex_;
+    }
+
+    int WmsSupportState::getCurrentOverlayIndex() const
+    {
+        return overlayIndex_;
+    }
+
+    void WmsSupportState::setBackgroundIndex(int index)
+    {
+        if(index < 0)
         {
-            backgroundsLength_ = static_cast<int>(setup.background.parsedServers.size());
-            overlaysLenght_ = static_cast<int>(setup.overlay.parsedServers.size());
+            layerGroupIndex_ = backgroundsLength_ - 1;
+        }
+        else if(index > (backgroundsLength_ - 1))
+        {
             layerGroupIndex_ = 0;
+        }
+        else
+        {
+            layerGroupIndex_ = index;
+        }
+    }
+
+    void WmsSupportState::setOverlayIndex(int index)
+    {
+        if(index < -1)
+        {
+            overlayIndex_ = overlaysLenght_ - 1;
+        }
+        else if(index > (overlaysLenght_ - 1))
+        {
             overlayIndex_ = -1;
         }
-
-        int getCurrentBackgroundIndex() const
+        else
         {
-            return layerGroupIndex_;
+            overlayIndex_ = index;
         }
+    }
 
-        int getCurrentOverlayIndex() const
-        {
-            return overlayIndex_;
-        }
-
-        void setBackgroundIndex(int index)
-        {
-            if(index < 0)
-            {
-                layerGroupIndex_ = backgroundsLength_ - 1;
-            }
-            else if(index > (backgroundsLength_ - 1))
-            {
-                layerGroupIndex_ = 0;
-            }
-            else
-            {
-                layerGroupIndex_ = index;
-            }
-        }
-
-        void setOverlayIndex(int index)
-        {
-            if(index < -1)
-            {
-                overlayIndex_ = overlaysLenght_ - 1;
-            }
-            else if(index > (overlaysLenght_ - 1))
-            {
-                overlayIndex_ = -1;
-            }
-            else
-            {
-                overlayIndex_ = index;
-            }
-        }
-    };
 }
