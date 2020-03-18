5caffc55be3fa9081ea1e7af6738a80267b9db05
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-03-18 15:09:13 +0200

    1. Added LocalOnlyMapModeUsedFunction type define, that is given as initialize method's parameter. With this function can be asked if SmartMet is in combined-map- or local-map-only -mode. 2. Added lastUsedLocalModeMapIndex_ data member, which is used as short term memory for combinedModeMapIndex_ variable. Using this memory complicates code, but I think it will be good feature. 3. Renamed isLocalMapInUse method to isLocalMapCurrentlyInUse

--- libs/fmi/bizcode/smetutils/include/NFmiCombinedMapModeState.h
+++ libs/fmi/bizcode/smetutils/include/NFmiCombinedMapModeState.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <functional>
+
 // T‰m‰ luokka tiet‰‰ eri moodien (lokaalit ja wms) karttojen lukum‰‰r‰t.
 // Voidaan menn‰ seuraavaan tai edelliseen karttaan ottaen huomioon yhteinen karttojen lukum‰‰r‰.
 // Voidaan asettaa suoraan haluttu yhteisindeksi.
@@ -7,23 +9,35 @@
 // Voidaan kysy‰ nykyhetken osoittaman kokonaisindeksin moodikohtainen (lokaali/wms) indeksi (currentMapSectionIndex metodi).
 class NFmiCombinedMapModeState
 {
+    using LocalOnlyMapModeUsedFunction = std::function<bool()>;
+
     int localMapSize_ = 0;
     int wmsMapSize_ = 0;
     int totalMapSize_ = 0;
     // Nollasta alkava indeksi, joka osoittaa johonkin totalMapSize_ koon rajoittamaan karttaan.
     int combinedModeMapIndex_ = 0;
+    // Nollasta alkava indeksi, joka osoittaa johonkin localMapSize_ koon rajoittamaan karttaan.
+    // T‰t‰ on tarkoitus p‰ivitt‰‰ (combinedModeMapIndex_:in ohessa) aina kun ollaan vain lokaali moodissa ja kun combinedModeMapIndex_ 
+    // on lokaalien indeksien rajoissa. T‰m‰n tarkoitus on olla lyhyt indeksi muisti, kun k‰ytt‰j‰ esim. hyppii local modesta combine moodiin ja takaisin.
+    int lastUsedLocalModeMapIndex_ = 0;
+    // T‰m‰n avulla kysyt‰‰n ollaanko lokaali vai combined-map moodissa.
+    LocalOnlyMapModeUsedFunction localOnlyMapModeUsedFunction_;
 public:
     NFmiCombinedMapModeState();
-    void initialize(int localMapSize, int wmsMapSize);
+    void initialize(int localMapSize, int wmsMapSize, LocalOnlyMapModeUsedFunction &localOnlyMapModeUsedFunction);
 
     void next();
     void previous();
     int combinedModeMapIndex() const;
     void combinedModeMapIndex(int index);
-    bool isLocalMapInUse() const;
+    bool isLocalMapCurrentlyInUse() const;
     int currentMapSectionIndex() const;
 
 private:
     void checkIndexUnderFlow();
     void checkIndexOverFlow();
+    bool isLocalOnlyMapModeInUse() const;
+    void mapIndexIsAboutToChangeChecks();
+    bool isMapIndexInLocalSection() const;
+    void updateLastUsedLocalModeMapIndex();
 };
