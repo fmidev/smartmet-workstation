8060d24d9587ca8bec8a32bb620408369812ffcd
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-03-18 15:09:13 +0200

    1. Added LocalOnlyMapModeUsedFunction type define, that is given as initialize method's parameter. With this function can be asked if SmartMet is in combined-map- or local-map-only -mode. 2. Added lastUsedLocalModeMapIndex_ data member, which is used as short term memory for combinedModeMapIndex_ variable. Using this memory complicates code, but I think it will be good feature. 3. Renamed isLocalMapInUse method to isLocalMapCurrentlyInUse

--- libs/fmi/bizcode/smetutils/source/NFmiCombinedMapModeState.cpp
+++ libs/fmi/bizcode/smetutils/source/NFmiCombinedMapModeState.cpp
@@ -2,21 +2,24 @@
 
 NFmiCombinedMapModeState::NFmiCombinedMapModeState() = default;
 
-void NFmiCombinedMapModeState::initialize(int localMapSize, int wmsMapSize)
+void NFmiCombinedMapModeState::initialize(int localMapSize, int wmsMapSize, LocalOnlyMapModeUsedFunction & localOnlyMapModeUsedFunction)
 {
     localMapSize_ = localMapSize;
     wmsMapSize_ = wmsMapSize;
     totalMapSize_ = localMapSize_ + wmsMapSize_;
+    localOnlyMapModeUsedFunction_ = localOnlyMapModeUsedFunction;
 }
 
 void NFmiCombinedMapModeState::next()
 {
+    mapIndexIsAboutToChangeChecks();
     combinedModeMapIndex_++;
     checkIndexOverFlow();
 }
 
 void NFmiCombinedMapModeState::previous()
 {
+    mapIndexIsAboutToChangeChecks();
     combinedModeMapIndex_--;
     checkIndexUnderFlow();
 }
@@ -33,30 +36,69 @@ void NFmiCombinedMapModeState::combinedModeMapIndex(int index)
     checkIndexUnderFlow();
 }
 
-bool NFmiCombinedMapModeState::isLocalMapInUse() const
+bool NFmiCombinedMapModeState::isLocalOnlyMapModeInUse() const
 {
-    if(combinedModeMapIndex_ < localMapSize_)
+    if(localOnlyMapModeUsedFunction_)
+        return localOnlyMapModeUsedFunction_();
+    else
+        return true;
+
+}
+
+bool NFmiCombinedMapModeState::isLocalMapCurrentlyInUse() const
+{
+    if(isLocalOnlyMapModeInUse())
+        return true;
+    else if(isMapIndexInLocalSection())
         return true;
     else
         return false;
 }
 
+bool NFmiCombinedMapModeState::isMapIndexInLocalSection() const
+{
+    return combinedModeMapIndex_ < localMapSize_;
+}
+
+// Tämän funktion on tehtävä varmistaa että jos ollaan local-only moodissa,
+// ja käyttäjä haluaa muuttaa karttaindeksiä (esim. next/previous)
+// että combinedModeMapIndex_ ja lastUsedLocalModeMapIndex_ ovat samoja, ennen 
+// kuin itse combinedModeMapIndex_:ia aletaan taas muuttamaan.
+void NFmiCombinedMapModeState::mapIndexIsAboutToChangeChecks()
+{
+    if(isLocalOnlyMapModeInUse())
+    {
+        if(combinedModeMapIndex_ != lastUsedLocalModeMapIndex_)
+            combinedModeMapIndex_ = lastUsedLocalModeMapIndex_;
+    }
+}
+
 int NFmiCombinedMapModeState::currentMapSectionIndex() const
 {
-    if(isLocalMapInUse())
-        return combinedModeMapIndex_;
+    if(isLocalOnlyMapModeInUse())
+        return lastUsedLocalModeMapIndex_; // Palautetaan lokaalin kartan indeksi 'erillismuistista'
+    else if(isMapIndexInLocalSection())
+        return combinedModeMapIndex_; // Ollaan combine moodissa, mutta lokaali sectiossa, palautetaan normi indeksi
     else
-        return combinedModeMapIndex_ - localMapSize_;
+        return combinedModeMapIndex_ - localMapSize_; // Palautetaan wms sectioon osoittava indeksi muutettuna sen omaksi 0-kantaiseksi indeksiksi
 }
 
 void NFmiCombinedMapModeState::checkIndexUnderFlow()
 {
     if(combinedModeMapIndex_ < 0)
         combinedModeMapIndex_ = totalMapSize_ - 1; // jos meni alle, mennään loppuun
+    updateLastUsedLocalModeMapIndex();
 }
 
 void NFmiCombinedMapModeState::checkIndexOverFlow()
 {
     if(combinedModeMapIndex_ >= totalMapSize_)
         combinedModeMapIndex_ = 0; // jos meni yli, palataan alkuun
+    updateLastUsedLocalModeMapIndex();
+}
+
+void NFmiCombinedMapModeState::updateLastUsedLocalModeMapIndex()
+{
+    if(isMapIndexInLocalSection())
+        lastUsedLocalModeMapIndex_ = combinedModeMapIndex_;
 }
