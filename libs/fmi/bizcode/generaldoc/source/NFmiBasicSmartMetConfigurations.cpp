4a065da308309e12fdc82aa3609635cc12760f5b
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-04-08 08:38:55 +0300

    Removed warning texts from the splash screen for next released version.

--- libs/fmi/bizcode/generaldoc/source/NFmiBasicSmartMetConfigurations.cpp
+++ libs/fmi/bizcode/generaldoc/source/NFmiBasicSmartMetConfigurations.cpp
@@ -524,11 +524,11 @@ void NFmiBasicSmartMetConfigurations::MakeSplashScreenTextDataVector(const NFmiT
     if(betaVersion)
         itsSplashScreenTextDataVector.push_back(DrawStringData(_TEXT("Beta"), _TEXT("Arial"), 25, RGB(0, 0, 0), CPoint(122, 267), true));
 
-    CString possibleWarningTextU_ = _TEXT("Paljon muutoksia mm. näyttöjen päivityskoodeihin!!");
+    CString possibleWarningTextU_; // = _TEXT("Paljon muutoksia mm. näyttöjen päivityskoodeihin!!");
     if(possibleWarningTextU_.GetLength())
         itsSplashScreenTextDataVector.push_back(DrawStringData(possibleWarningTextU_, _TEXT("Arial"), 25, RGB(255, 0, 0), CPoint(22, 370), true));
 
-    CString possibleWarningText2U_ = _TEXT("Raportoi tähän versioon liittyvistä ongelmista pikaisesti!");
+    CString possibleWarningText2U_; // = _TEXT("Raportoi tähän versioon liittyvistä ongelmista pikaisesti!");
     if(possibleWarningText2U_.GetLength())
         itsSplashScreenTextDataVector.push_back(DrawStringData(possibleWarningText2U_, _TEXT("Arial"), 25, RGB(255, 0, 0), CPoint(22, 395), true));
 }
