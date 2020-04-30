18c53f247c3617b2398134acb31f5aefac4e0b21
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-04-30 08:47:33 +0300

    Renamed CtrlViewDocumentInterface classes CalculateSubMapViewHeightInMilliMeters method to SingleMapViewHeightInMilliMeters, because it's just returns the value instead of calculating it.

--- libs/fmi/bizcode/generaldoc/include/CtrlViewDocumentInterfaceForGeneralDataDoc.h
+++ libs/fmi/bizcode/generaldoc/include/CtrlViewDocumentInterfaceForGeneralDataDoc.h
@@ -291,7 +291,7 @@ public:
     bool BorderDrawPolylinesDirty(int theDescTopIndex) const override;
     bool BorderDrawPolylinesGdiplusDirty(int theDescTopIndex) const override;
     void SetBorderDrawDirtyState(int theDescTopIndex, CountryBorderDrawDirtyState newState) override;
-    double CalculateSubMapViewHeightInMilliMeters(int theDescTopIndex) override;
+    double SingleMapViewHeightInMilliMeters(int theDescTopIndex) const override;
 
 #ifndef DISABLE_CPPRESTSDK
     HakeMessage::Main& WarningCenterSystem(void) override;
