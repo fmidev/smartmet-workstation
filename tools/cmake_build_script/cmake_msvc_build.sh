ca1342532a9fefe267a812749f0ae39812b29279
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-05-13 08:47:39 +0300

    Fixed BaseProjectDir to "smartmet-workstation-wgs84"

--- tools/cmake_build_script/cmake_msvc_build.sh
+++ tools/cmake_build_script/cmake_msvc_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BaseProjectDir="smartmet-workstation"
+BaseProjectDir="smartmet-workstation-wgs84"
 
 cd ../../.. && mkdir -p ${BaseProjectDir}_msvc && cd ${BaseProjectDir}_msvc && cmake -G "Visual Studio 16 2019" -A x64 ../$BaseProjectDir
 
