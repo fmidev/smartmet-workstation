a673046d230bd6945bd506f8840e410ae1d6f067
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-05-13 08:48:10 +0300

    Added smartmet-library-gis library formatting

--- tools/gitflow/github_modules_clang_format.sh
+++ tools/gitflow/github_modules_clang_format.sh
@@ -4,11 +4,15 @@ clang_format_exe_path="/d/Program Files (x86)/Microsoft Visual Studio/2019/Profe
 
 cd ../../github_modules
 
-# First newbase
+# First gis
+gis_dir="smartmet-library-gis"
+[ -d "$gis_dir" ] && "$clang_format_exe_path" -i -style=file -verbose "$gis_dir"/gis/*.cpp "$gis_dir"/gis/*.h
+
+# Second newbase
 newbase_dir="smartmet-library-newbase"
 [ -d "$newbase_dir" ] && "$clang_format_exe_path" -i -style=file -verbose "$newbase_dir"/newbase/*.cpp "$newbase_dir"/newbase/*.h
 
-# Second smarttools
+# Third smarttools
 smarttools_dir="smartmet-library-smarttools"
 [ -d "$smarttools_dir" ] && "$clang_format_exe_path" -i -style=file -verbose "$smarttools_dir"/smarttools/*.cpp "$smarttools_dir"/smarttools/*.h
 
