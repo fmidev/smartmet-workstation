0cdcffcf32b15c91c67a03ad2e30bc46a479f90d
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-06-15 10:11:21 +0300

    Changed BaseProjectDir to smartmet-workstation (was smartmet-workstation-wgs84), now the new build system is in new ver200_SmartMet_release_5_14 directory (was side by side with old one in ver200_SmartMet_release_5_13 directory).

--- tools/cmake_build_script/cmake_msvc_build.sh
+++ tools/cmake_build_script/cmake_msvc_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BaseProjectDir="smartmet-workstation-wgs84"
+BaseProjectDir="smartmet-workstation"
 
 cd ../../.. && mkdir -p ${BaseProjectDir}_msvc && cd ${BaseProjectDir}_msvc && cmake -G "Visual Studio 16 2019" -A x64 ../$BaseProjectDir
 
