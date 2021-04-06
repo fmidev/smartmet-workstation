32d97e25c226c7c2a976cb5902e51a87e76f27f5
Marko Pietarinen <marko.pietarinen@fmi.fi>
2021-04-06 11:55:19 +0300

    1. Fifth simple contour color was added (itsColorContouringColorShadeHigh3ValueColor) 2. Added transparency option for all 5 simple contour colors. 3. Removed if(itsContourGab == 0) check from ContourGab setter method, because 0 gab means that no color blending is done from now on (and negative values too). 4. New values are added to extra read/write sections.  - If old version's data is read, the 5th simple contour color will get 4th color Value  - And all new transparency options are set to false. 5. Default values for simple contour colors 1-5 are now set as blue,green,red,magenta,magenta

--- modules/smarttools/smarttools/NFmiDrawParam.cpp
+++ modules/smarttools/smarttools/NFmiDrawParam.cpp
@@ -35,6 +35,7 @@
 #include "NFmiDataStoringHelpers.h"
 
 #include <fstream>
+#include <bitset>
 
 const NFmiColor gLabelBoxDefaultFillColor(1., 1., 0.);  // keltainen
 
@@ -138,8 +139,9 @@ NFmiDrawParam::NFmiDrawParam()
       itsColorContouringColorShadeHigh2Value(100),
       itsColorContouringColorShadeLowValueColor(0, 0, 1),
       itsColorContouringColorShadeMidValueColor(0, 1, 0),
-      itsColorContouringColorShadeHighValueColor(0, 1, 0),
-      itsColorContouringColorShadeHigh2ValueColor(0, 1, 0),
+      itsColorContouringColorShadeHighValueColor(1, 0, 0),
+      itsColorContouringColorShadeHigh2ValueColor(0.5f, 0, 1),
+      itsColorContouringColorShadeHigh3ValueColor(0.5f, 0, 1),
       fUseWithIsoLineHatch1(false),
       fDrawIsoLineHatchWithBorders1(false),
       itsIsoLineHatchLowValue1(0),
@@ -280,8 +282,9 @@ NFmiDrawParam::NFmiDrawParam(const NFmiDataIdent& theParam,
       itsColorContouringColorShadeHigh2Value(100),
       itsColorContouringColorShadeLowValueColor(0, 0, 1),
       itsColorContouringColorShadeMidValueColor(0, 1, 0),
-      itsColorContouringColorShadeHighValueColor(0, 1, 0),
-      itsColorContouringColorShadeHigh2ValueColor(0, 1, 0),
+      itsColorContouringColorShadeHighValueColor(1, 0, 0),
+      itsColorContouringColorShadeHigh2ValueColor(0.5f, 0, 1),
+      itsColorContouringColorShadeHigh3ValueColor(0.5f, 0, 1),
       fUseWithIsoLineHatch1(false),
       fDrawIsoLineHatchWithBorders1(false),
       itsIsoLineHatchLowValue1(0),
@@ -423,6 +426,13 @@ NFmiDrawParam::NFmiDrawParam(const NFmiDrawParam& other)
       itsColorContouringColorShadeHighValueColor(other.itsColorContouringColorShadeHighValueColor),
       itsColorContouringColorShadeHigh2ValueColor(
           other.itsColorContouringColorShadeHigh2ValueColor),
+      itsColorContouringColorShadeHigh3ValueColor(
+          other.itsColorContouringColorShadeHigh3ValueColor),
+      fSimpleColorContourTransparentColor1(other.fSimpleColorContourTransparentColor1),
+      fSimpleColorContourTransparentColor2(other.fSimpleColorContourTransparentColor2),
+      fSimpleColorContourTransparentColor3(other.fSimpleColorContourTransparentColor3),
+      fSimpleColorContourTransparentColor4(other.fSimpleColorContourTransparentColor4),
+      fSimpleColorContourTransparentColor5(other.fSimpleColorContourTransparentColor5),
       fUseWithIsoLineHatch1(other.fUseWithIsoLineHatch1),
       fDrawIsoLineHatchWithBorders1(other.fDrawIsoLineHatchWithBorders1),
       itsIsoLineHatchLowValue1(other.itsIsoLineHatchLowValue1),
@@ -611,6 +621,13 @@ void NFmiDrawParam::Init(const NFmiDrawParam* theDrawParam, bool fInitOnlyDrawin
         theDrawParam->ColorContouringColorShadeHighValueColor();
     itsColorContouringColorShadeHigh2ValueColor =
         theDrawParam->ColorContouringColorShadeHigh2ValueColor();
+    itsColorContouringColorShadeHigh3ValueColor =
+        theDrawParam->ColorContouringColorShadeHigh3ValueColor();
+    fSimpleColorContourTransparentColor1 = theDrawParam->fSimpleColorContourTransparentColor1;
+    fSimpleColorContourTransparentColor2 = theDrawParam->fSimpleColorContourTransparentColor2;
+    fSimpleColorContourTransparentColor3 = theDrawParam->fSimpleColorContourTransparentColor3;
+    fSimpleColorContourTransparentColor4 = theDrawParam->fSimpleColorContourTransparentColor4;
+    fSimpleColorContourTransparentColor5 = theDrawParam->fSimpleColorContourTransparentColor5;
     fUseWithIsoLineHatch1 = theDrawParam->UseWithIsoLineHatch1();
     fDrawIsoLineHatchWithBorders1 = theDrawParam->DrawIsoLineHatchWithBorders1();
     itsIsoLineHatchLowValue1 = theDrawParam->IsoLineHatchLowValue1();
@@ -735,6 +752,48 @@ static NFmiMetTime String2MetTime(const std::string& theStr)
   NFmiMetTime tmpTime;
   tmpTime.FromStr(theStr, gMetTime2ViewMacroStringFormat);
   return tmpTime;
+}
+static std::string Color2String(const NFmiColor& theColor)
+{
+  std::stringstream out;
+  out << theColor;
+  return out.str();
+}
+static NFmiColor String2Color(const std::string& theColorString)
+{
+  std::stringstream in(theColorString);
+  NFmiColor color;
+  in >> color;
+  return color;
+}
+
+// Function makes from simple-contour 5 colors transparency boolean values
+// (fSimpleColorContourTransparentColor1-5 data members)
+// to bitset and that is converted to double to be stored in 
+// NFmiDrawParam::Write method with NFmiDataStoringHelpers::NFmiExtraDataStorage
+// as double value. Alternative was to store 5 separate bool values as 5
+// double values.
+double NFmiDrawParam::SimpleColorContourTransparentColors2Double() const
+{
+  std::bitset<5> value;
+  if (fSimpleColorContourTransparentColor1) value[0] = true;
+  if (fSimpleColorContourTransparentColor2) value[1] = true;
+  if (fSimpleColorContourTransparentColor3) value[2] = true;
+  if (fSimpleColorContourTransparentColor4) value[3] = true;
+  if (fSimpleColorContourTransparentColor5) value[4] = true;
+  return static_cast<double>(value.to_ulong());
+}
+
+// This converts given double value to bitset and it's first
+// 5 bit are given to each fSimpleColorContourTransparentColor1-5 data members.
+void NFmiDrawParam::Double2SimpleColorContourTransparentColors(double theValue) 
+{
+  std::bitset<5> bitValue(static_cast<unsigned long>(theValue));
+  fSimpleColorContourTransparentColor1 = bitValue[0];
+  fSimpleColorContourTransparentColor2 = bitValue[1];
+  fSimpleColorContourTransparentColor3 = bitValue[2];
+  fSimpleColorContourTransparentColor4 = bitValue[3];
+  fSimpleColorContourTransparentColor5 = bitValue[4];
 }
 
 //--------------------------------------------------------
@@ -1015,9 +1074,14 @@ std::ostream& NFmiDrawParam::Write(std::ostream& file) const
     extraData.Add(static_cast<double>(fUseTransparentFillColor));
     // fDoSparseSymbolVisualization on 8. uusista double-extra-parametreista
     extraData.Add(static_cast<double>(fDoSparseSymbolVisualization));
+    // fSimpleColorContourTransparentColor1-5 arvoista tehdään 9. uusista double-extra-parametreista
+    extraData.Add(SimpleColorContourTransparentColors2Double());
 
     // modelRunIndex on 1. uusista string-extra-parametreista
     extraData.Add(::MetTime2String(itsModelOriginTime));
+    // 5. simple color contour väri (itsColorContouringColorShadeHigh3ValueColor)
+    // on 2. uusista string-extra-parametreista
+    extraData.Add(::Color2String(itsColorContouringColorShadeHigh3ValueColor));
 
     file << "possible_extra_data" << std::endl;
     file << extraData;
@@ -1395,12 +1459,28 @@ std::istream& NFmiDrawParam::Read(std::istream& file)
         {
           fDoSparseSymbolVisualization = extraData.itsDoubleValues[7] != 0;
         }
+        double simpleContourColorsTransparencyBits = 0; // Oletusarvona kaikki laitetaan ei-transparenteiksi
+        if (extraData.itsDoubleValues.size() >= 9)
+        {
+          simpleContourColorsTransparencyBits = extraData.itsDoubleValues[8];
+        }
+        Double2SimpleColorContourTransparentColors(simpleContourColorsTransparencyBits);
 
         itsModelOriginTime = NFmiMetTime::gMissingTime;  // tämä on oletus arvo eli ei ole käytössä
         if (extraData.itsStringValues.size() >= 1)
         {
           // laitetaan asetus-funktion läpi, jossa raja tarkistukset
           ModelOriginTime(::String2MetTime(extraData.itsStringValues[0]));
+        }
+
+        if (extraData.itsStringValues.size() >= 2)
+        {
+          itsColorContouringColorShadeHigh3ValueColor = ::String2Color(extraData.itsStringValues[1]);
+        }
+        else
+        {
+          // Jos luetaan vanhan version tekemää drawParamia, kopsataan vain 4. väri 5. väriksi
+          itsColorContouringColorShadeHigh3ValueColor = itsColorContouringColorShadeHigh2ValueColor;
         }
 
         if (file.fail()) throw std::runtime_error("NFmiDrawParam::Read failed");
@@ -1412,6 +1492,12 @@ std::istream& NFmiDrawParam::Read(std::istream& file)
       {  // tietyt muuttujat pitää alustaa jos versio 2.
         itsColorContouringColorShadeHigh2Value = itsColorContouringColorShadeHighValue;
         itsColorContouringColorShadeHigh2ValueColor = itsColorContouringColorShadeHighValueColor;
+        itsColorContouringColorShadeHigh3ValueColor = itsColorContouringColorShadeHighValueColor;
+        fSimpleColorContourTransparentColor1 = false;
+        fSimpleColorContourTransparentColor2 = false;
+        fSimpleColorContourTransparentColor3 = false;
+        fSimpleColorContourTransparentColor4 = false;
+        fSimpleColorContourTransparentColor5 = false;
 
         itsContourLabelBoxFillColor = itsIsolineLabelBoxFillColor;
         fUseContourGabWithCustomContours = false;
