d850a1818f08df38c39504c6eef76c28130b8991
Marko Pietarinen <marko.pietarinen@fmi.fi>
2021-05-11 15:23:07 +0300

    Added --rebase to pull scripts

--- tools/gitflow/pull_dev.sh
+++ tools/gitflow/pull_dev.sh
@@ -2,6 +2,6 @@
 
 dev=$(cat ./branches/dev_branch)
 
-git checkout $dev && git pull --tags --no-edit origin $dev
+git checkout $dev && git pull --rebase --tags --no-edit origin $dev
 
 read -p "Press [Enter] key to continue..."
