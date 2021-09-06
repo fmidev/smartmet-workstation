7342a0f32cd42a512c2c27601e4f9e4d43c23fd8
Marko Pietarinen <mpietarin@gmail.com>
2021-09-06 18:35:28 +0300

    Fixed NFmiDataLoadingInfo to fix all paths so that slashes are correct, absolute paths have correct drive letters and there is no relative path jumps. Originally noticed that itsWorkingPath data member had absolute path, but no drive letter in it (this could cause random results when saving files).

--- libs/fmi/bizcode/generaldoc/source/NFmiEditMapGeneralDataDoc.cpp
+++ libs/fmi/bizcode/generaldoc/source/NFmiEditMapGeneralDataDoc.cpp
@@ -681,7 +681,7 @@ void InitDataLoadingInfo()
 {
     try
     {
-        itsDataLoadingInfoNormal.Configure();
+        itsDataLoadingInfoNormal.Configure(WorkingDirectory());
         AddWorkingDataPathToCleaner(); // HUOM! DataLoadingInfo ja FileCleanerSystem molemmat pit‰‰ olla initilaisoituina!!!!
         itsDataLoadingInfoCaseStudy = itsDataLoadingInfoNormal; // otetaan CaseStudy dataLoadingInfon pohjatiedot normaalista dataLoadingInfosta
     }
