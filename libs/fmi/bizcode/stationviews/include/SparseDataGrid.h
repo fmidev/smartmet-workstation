c60c545f4e7696d59b2feb0819bfd0f5676eba79
Marko Pietarinen <mpietarin@gmail.com>
2021-12-03 08:16:39 +0200

    Improved sparse data visualization by eliminating too close 'same-sided' pair hits. Now possible multiple peekIndex points are stored for each SparseData (when they all are at the same shortest distance), and we seek best match from them when doing horizontalElimination and verticalElimination checks.

--- libs/fmi/bizcode/stationviews/include/SparseDataGrid.h
+++ libs/fmi/bizcode/stationviews/include/SparseDataGrid.h
@@ -8,25 +8,26 @@ class SparseData
 {
 	float value_ = kFloatMissing;
 	NFmiPoint drawGridPoint_ = NFmiPoint::gMissingLatlon;
-	NFmiPoint peekIndex_ = NFmiPoint::gMissingLatlon;
+	std::list<NFmiPoint> peekIndexList_;
 	unsigned long visualizedLocationIndex_ = gMissingIndex;
 	bool insideZoomedArea_ = false;
 public:
 	SparseData();
-	SparseData(float value, const NFmiPoint& drawGridPoint, const NFmiPoint &peekIndex, unsigned long visualizedLocationIndex);
+	SparseData(float value, const NFmiPoint& drawGridPoint, const std::list<NFmiPoint> &peekIndexList, unsigned long visualizedLocationIndex);
 	
 	void clearData();
 	bool hasValue() const;
 	bool peekIndexIsCloserToCenter(const SparseData& otherData) const;
 	double calcPeekDistance() const;
-	FmiDirection peekIndexDirection() const;
-	const NFmiPoint& peekIndex() const { return peekIndex_; }
+	NFmiPoint seekPeekIndexFromDirection(FmiDirection wantedDirection) const;
+	const std::list<NFmiPoint>& peekIndexList() const { return peekIndexList_; }
 	float value() const { return value_; }
 	unsigned long visualizedLocationIndex() const { return visualizedLocationIndex_; }
 	bool insideZoomedArea() const { return insideZoomedArea_; }
 	static FmiDirection horizontalElimination(const SparseData &leftData, const SparseData &rightData);
 	static FmiDirection verticalElimination(const SparseData& lowerData, const SparseData& upperData);
 	static double drawGridPointDistance(const SparseData &data1, const SparseData &data2);
+	static FmiDirection calcPeekIndexDirection(const NFmiPoint& peekIndex);
 };
 
 class SparseDataGrid
