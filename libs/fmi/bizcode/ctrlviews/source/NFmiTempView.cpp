fmidev/smartmet-workstation

commit c85b6ccb3ba0c4dde577e1a05df83f57edf1a858

Resetting itsFirstSoundingData data member in NFmiTempView::Draw method, so that last good first-sounding won't be used later in textual sub-views (stability-index-view or sounding-data-in-text-format-view). This problem occurred if first you select sounding were data is found and then you would select sounding from time that's outside of data's time range.

libs/fmi/bizcode/ctrlviews/source/NFmiTempView.cpp
@@ -739,8 +739,9 @@ void NFmiTempView::Draw(NFmiToolBox *theToolBox)
             itsDrawingEnvironment->SetFillColor(NFmiColor(0.9f, 0.9f, 0.9f)); // laitetaan harmaa tausta teksteille, että ne erottuu
             itsDrawingEnvironment->SetFontSize(NFmiPoint(18 * itsDrawSizeFactorX, 18 * itsDrawSizeFactorY));
             itsDrawingEnvironment->SetPenSize(NFmiPoint(1 * itsDrawSizeFactorX, 1 * itsDrawSizeFactorY));
-
-            DrawSoundingsInMTAMode();
+			// Resetoidaan itsFirstSoundingData, muuten tulee ongelmia viimeisen 'hyvän' 1. datan piirrossa tekstimuotoisiin ikkunoihin, jos tässä piirrossa ei löydy dataa ollenkaan (esim. ollaan 1. datan aikaikkunan ulkopuolella)
+			itsFirstSoundingData = NFmiSoundingDataOpt1(); 
+			DrawSoundingsInMTAMode();
 
             // siivotaan piirto ominaisuudet takaisin
             itsDrawingEnvironment->SetFillColor(oldFillColor);

commit a95adb6544a64cf46eb696f10d1c08ae9439d3b2

Fixing can't modify the 1st sounding with mouse bug. Bug was introduced when fixing other bug relating to 'ghost' values in side-text-views when no data was available to the current 1st sounding producer/time/location. Bug was introduced with c85b6ccb3ba0c4dde577e1a05df83f57edf1a858 commit on 20th of August 2020. Now you can modify the 1st sounding with mouse and there are no 'ghosts' in side-text-views.

libs/fmi/bizcode/ctrlviews/source/NFmiTempView.cpp
@@ -691,8 +691,6 @@ void NFmiTempView::Draw(NFmiToolBox *theToolBox)
             itsDrawingEnvironment->SetFillColor(NFmiColor(0.9f, 0.9f, 0.9f)); // laitetaan harmaa tausta teksteille, että ne erottuu
             itsDrawingEnvironment->SetFontSize(NFmiPoint(18 * itsDrawSizeFactor.X(), 18 * itsDrawSizeFactor.Y()));
             itsDrawingEnvironment->SetPenSize(NFmiPoint(1 * itsDrawSizeFactor.X(), 1 * itsDrawSizeFactor.Y()));
-			// Resetoidaan itsFirstSoundingData, muuten tulee ongelmia viimeisen 'hyvän' 1. datan piirrossa tekstimuotoisiin ikkunoihin, jos tässä piirrossa ei löydy dataa ollenkaan (esim. ollaan 1. datan aikaikkunan ulkopuolella)
-			itsFirstSoundingData = NFmiSoundingDataOpt1(); 
 			DrawSoundingsInMTAMode();
 
             // siivotaan piirto ominaisuudet takaisin
@@ -1970,6 +1968,15 @@ void NFmiTempView::DrawOneSounding(const NFmiMTATempSystem::SoundingProducer &th
         bool onSouthernHemiSphere = usedTempInfo.Latlon().Y() < 0;
 		DrawSounding(sounding, theIndex, usedColor, mainCurve, onSouthernHemiSphere);
         itsSoundingDataCacheForTooltips.insert(std::make_pair(NFmiMTATempSystem::SoundingDataCacheMapKey(usedTempInfo, theProducer, theModelRunIndex), sounding));
+	}
+	else
+	{
+		if(theIndex == 0)
+		{
+			// Jos ei löytynyt mitään dataa ja kyse oli 1. piirrettävästä luotausdatasta, pitää itsFirstSoundingData dataosa nollata,
+			// jotta tekstimuotoisiin sivunäyttöihin ei jäisi vanha data 'kummittelemaan'
+			itsFirstSoundingData = NFmiSoundingDataOpt1();
+		}
 	}
 }
 