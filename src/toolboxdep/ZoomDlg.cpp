289b49f37a23d9c000269ce52c67181018c1e13e
Marko Pietarinen <mpietarin@gmail.com>
2022-12-05 16:52:28 +0200

    Changed zooming dialog following ways: - Before only one selected map-view (1-3) could be manipulated at a time - Now radio-button selector is replaced with three check-boxes, one for each map-view - Depending on check-box selections zooming is applied to 0-3 map-views at once - If "Allow mixed map area zoom" option is not checked, only same base map area views are allowed to be zoomed together - If "Allow mixed map area zoom" option is checked, then if the actual zooming map-view base map area is inside the other map view's base map area, then mixed area zooming is possible - Without checking area against each others "Allow mixed map area zoom" option could lead zooming where zoomed area is out of base map area and only white colour would be visualized there

--- src/toolboxdep/ZoomDlg.cpp
+++ src/toolboxdep/ZoomDlg.cpp
@@ -16,12 +16,15 @@
 #include "FmiWin32Helpers.h"
 #include "NFmiApplicationWinRegistry.h"
 #include "persist2.h"
+#include "NFmiArea.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #undef THIS_FILE
 static char THIS_FILE[] = __FILE__;
 #endif
+
+const unsigned int gNonMapViewWasSelectedIndex = 99999;
 
 /////////////////////////////////////////////////////////////////////////////
 // CZoomDlg dialog
@@ -31,7 +34,10 @@ CZoomDlg::CZoomDlg(SmartMetDocumentInterface *smartMetDocumentInterface, CWnd* p
 : CDialog(CZoomDlg::IDD, pParent)
 ,itsClientView(0)
 ,itsSmartMetDocumentInterface(smartMetDocumentInterface)
-,itsSelectedMapViewDescTopIndex(0)
+, fMapView1Enabled(TRUE)
+, fMapView2Enabled(FALSE)
+, fMapView3Enabled(FALSE)
+, fAllowMixedMapAreaZoom(FALSE)
 {
 //	itsMapHandler->SaveState();
 	//{{AFX_DATA_INIT(CZoomDlg)
@@ -45,7 +51,10 @@ void CZoomDlg::DoDataExchange(CDataExchange* pDX)
 	//{{AFX_DATA_MAP(CZoomDlg)
 	// NOTE: the ClassWizard will add DDX and DDV calls here
 	//}}AFX_DATA_MAP
-	DDX_Radio(pDX, IDC_RADIO_ZOOM_MAP_VIEW1, itsSelectedMapViewDescTopIndex);
+	DDX_Check(pDX, IDC_CHECK_MAP_VIEW1_ENABLED, fMapView1Enabled);
+	DDX_Check(pDX, IDC_CHECK_MAP_VIEW2_ENABLED, fMapView2Enabled);
+	DDX_Check(pDX, IDC_CHECK_MAP_VIEW3_ENABLED, fMapView3Enabled);
+	DDX_Check(pDX, IDC_CHECK_ALLOW_MIXED_MAP_AREA_ZOOMING, fAllowMixedMapAreaZoom);
 }
 
 
@@ -54,9 +63,10 @@ BEGIN_MESSAGE_MAP(CZoomDlg, CDialog)
 	ON_WM_SIZE()
 	ON_BN_CLICKED(ID_SHOW_ZOOMED_AREA, OnShowZoomedArea)
 	//}}AFX_MSG_MAP
-	ON_BN_CLICKED(IDC_RADIO_ZOOM_MAP_VIEW1, OnBnClickedRadioZoomMapView1)
-	ON_BN_CLICKED(IDC_RADIO_ZOOM_MAP_VIEW2, OnBnClickedRadioZoomMapView2)
-	ON_BN_CLICKED(IDC_RADIO_ZOOM_MAP_VIEW3, OnBnClickedRadioZoomMapView3)
+	ON_BN_CLICKED(IDC_CHECK_MAP_VIEW1_ENABLED, &CZoomDlg::OnBnClickedCheckMapView1Enabled)
+	ON_BN_CLICKED(IDC_CHECK_MAP_VIEW2_ENABLED, &CZoomDlg::OnBnClickedCheckMapView2Enabled)
+	ON_BN_CLICKED(IDC_CHECK_MAP_VIEW3_ENABLED, &CZoomDlg::OnBnClickedCheckMapView3Enabled)
+	ON_BN_CLICKED(IDC_CHECK_ALLOW_MIXED_MAP_AREA_ZOOMING, &CZoomDlg::OnBnClickedCheckAllowMixedMapAreaZooming)
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -119,24 +129,52 @@ void CZoomDlg::MoveMapArea()
 		itsClientView->MoveWindow(rect);
 }
 
+unsigned int CZoomDlg::GetFirstSelectedMapViewIndex()
+{
+	if(fMapView1Enabled)
+		return 0;
+	if(fMapView2Enabled)
+		return 1;
+	if(fMapView3Enabled)
+		return 2;
+	return gNonMapViewWasSelectedIndex;
+}
+
+int CZoomDlg::CalcClientRectTopYPos()
+{
+	auto ctrlWin = GetDlgItem(IDC_CHECK_ALLOW_MIXED_MAP_AREA_ZOOMING);
+	if(ctrlWin)
+	{
+		WINDOWPLACEMENT wplace;
+		ctrlWin->GetWindowPlacement(&wplace);
+		return wplace.rcNormalPosition.bottom + 1;
+	}
+	const int pixelRoomForButtons = 35; // room for all constrols at top of view
+	return pixelRoomForButtons;
+}
+
 CRect CZoomDlg::CalcMapArea()
 {
-	const int pixelRoomForButtons = 35; // room for OK and Cancel buttons
-	const int roomForEdges = 5; // map will occupy all clientarea except this from the edges
+	auto yPos = CalcClientRectTopYPos();
+	const int roomForEdges = 3; // map will occupy all clientarea except this from the edges
 	CRect clientRect;
 	GetClientRect(clientRect);
 	CRect mapRect(clientRect.left + roomForEdges,
-				  clientRect.top + pixelRoomForButtons,
+				  clientRect.top + yPos,
 				  clientRect.right - roomForEdges,
 				  clientRect.bottom - roomForEdges);
 
     if(itsSmartMetDocumentInterface && itsSmartMetDocumentInterface->ApplicationWinRegistry().KeepMapAspectRatio())
 	{
-		NFmiRect rect2(mapRect.left, mapRect.top, mapRect.right, mapRect.bottom);
-		double aspectRatio = itsSmartMetDocumentInterface->MapViewDescTop(itsSelectedMapViewDescTopIndex)->MapHandler()->BitmapAspectRatio();
-		rect2.AdjustAspectRatio(aspectRatio, true, kTopLeft);
-		CRect newRect((int)rect2.Left(), (int)rect2.Top(), (int)rect2.Right(), (int)rect2.Bottom());
-		mapRect = newRect;
+		auto selectedMapViewIndex = GetFirstSelectedMapViewIndex();
+		if(selectedMapViewIndex != gNonMapViewWasSelectedIndex)
+		{
+			NFmiRect rect2(mapRect.left, mapRect.top, mapRect.right, mapRect.bottom);
+			double aspectRatio = itsSmartMetDocumentInterface->MapViewDescTop(selectedMapViewIndex)->MapHandler()->BitmapAspectRatio();
+			rect2.AdjustAspectRatio(aspectRatio, true, kTopLeft);
+			CRect newRect((int)rect2.Left(), (int)rect2.Top(), (int)rect2.Right(), (int)rect2.Bottom());
+			mapRect = newRect;
+		}
 	}
 
 	return mapRect;
@@ -151,8 +189,12 @@ void CZoomDlg::Update(void)
     if(IsWindowVisible() && !IsIconic()) // Näyttöä päivitetään vain jos se on näkyvissä ja se ei ole minimized tilassa
     {
         UpdateData(TRUE);
-        if(itsClientView)
-            ((CZoomView*)itsClientView)->Update(itsSelectedMapViewDescTopIndex);
+		auto selectedMapViewIndex = GetFirstSelectedMapViewIndex();
+		if(selectedMapViewIndex != gNonMapViewWasSelectedIndex)
+		{
+			if(itsClientView)
+				((CZoomView*)itsClientView)->Update(selectedMapViewIndex);
+		}
         MoveMapArea();
         Invalidate(FALSE);
     }
@@ -165,9 +207,67 @@ void CZoomDlg::OnShowZoomedArea()
 
 void CZoomDlg::Zoom(void)
 {
-    CatLog::logMessage("Zooming map view.", CatLog::Severity::Info, CatLog::Category::Visualization);
-    itsSmartMetDocumentInterface->SetMapArea(itsSelectedMapViewDescTopIndex, ((CZoomView*)itsClientView)->ZoomedArea());
-    itsSmartMetDocumentInterface->RefreshApplicationViewsAndDialogs(__FUNCTION__);
+	Zoom(0, fMapView1Enabled);
+	Zoom(1, fMapView2Enabled);
+	Zoom(2, fMapView3Enabled);
+	itsSmartMetDocumentInterface->RefreshApplicationViewsAndDialogs(__FUNCTION__);
+}
+
+bool CZoomDlg::CheckIfMapAreaZoomIsAllowed(unsigned int selectedMapViewIndex)
+{
+	auto mapViewIndexUsedInZooming = GetFirstSelectedMapViewIndex();
+	if(selectedMapViewIndex == mapViewIndexUsedInZooming)
+		return true;
+
+	auto zoomingMapAreaIndex = itsSmartMetDocumentInterface->MapViewDescTop(mapViewIndexUsedInZooming)->SelectedMapIndex();
+	auto selectedMapAreaIndex = itsSmartMetDocumentInterface->MapViewDescTop(selectedMapViewIndex)->SelectedMapIndex();
+	if(fAllowMixedMapAreaZoom)
+	{
+		// Sallittu, jos zoomaus kartan peruskartta-alue on zoomatun peruskartta-alueen sisällä
+		auto zoomingBaseMapArea = itsSmartMetDocumentInterface->GetMapHandlerInterface(mapViewIndexUsedInZooming)->TotalArea();
+		auto selectedMapBaseMapArea = itsSmartMetDocumentInterface->GetMapHandlerInterface(selectedMapViewIndex)->TotalArea();
+		bool zoomingAllowed = selectedMapBaseMapArea->IsInside(*zoomingBaseMapArea);
+		if(!zoomingAllowed)
+		{
+			std::string logStr = "Zooming is not allowed for map-view-";
+			logStr += std::to_string(selectedMapViewIndex + 1);
+			logStr += " because map-view-";
+			logStr += std::to_string(mapViewIndexUsedInZooming + 1);
+			logStr += " has different base map area and the  these two areas won't cover each others correctly and zooming might cause problems";
+			CatLog::logMessage(logStr, CatLog::Severity::Debug, CatLog::Category::Visualization);
+		}
+		return zoomingAllowed;
+	}
+
+	// Sallittua vain jos eri kartoilla on samat peruskartta-alueet käytössä
+	bool zoomingAllowed = (zoomingMapAreaIndex == selectedMapAreaIndex);
+	if(!zoomingAllowed)
+	{
+		std::string logStr = "Zooming is not allowed for map-view-";
+		logStr += std::to_string(selectedMapViewIndex + 1);
+		logStr += " because map-view-";
+		logStr += std::to_string(mapViewIndexUsedInZooming + 1);
+		logStr += " has different base map area and the \"Allow mixed map area zoom\" option is not set on";
+		CatLog::logMessage(logStr, CatLog::Severity::Debug, CatLog::Category::Visualization);
+	}
+	return zoomingAllowed;
+}
+
+void CZoomDlg::Zoom(unsigned int selectedMapViewIndex, BOOL mapViewEnabled)
+{
+	if(mapViewEnabled)
+	{
+		if(CheckIfMapAreaZoomIsAllowed(selectedMapViewIndex))
+		{
+			std::string logStr = "Zooming map view ";
+			logStr += std::to_string(selectedMapViewIndex + 1);
+			logStr += " with area: ";
+			auto zoomedArea = ((CZoomView*)itsClientView)->ZoomedArea();
+			logStr += zoomedArea->AreaStr();
+			CatLog::logMessage(logStr, CatLog::Severity::Info, CatLog::Category::Visualization);
+			itsSmartMetDocumentInterface->SetMapArea(selectedMapViewIndex, zoomedArea);
+		}
+	}
 }
 
 void CZoomDlg::SetDefaultValues(void)
@@ -186,28 +286,44 @@ void CZoomDlg::InitDialogTexts(void)
 	CFmiWin32Helpers::SetDialogItemText(this, IDOK, "IDOK");
 	CFmiWin32Helpers::SetDialogItemText(this, IDCANCEL, "IDCANCEL");
 	CFmiWin32Helpers::SetDialogItemText(this, ID_SHOW_ZOOMED_AREA, "ID_SHOW_ZOOMED_AREA");
-	CFmiWin32Helpers::SetDialogItemText(this, IDC_STATIC_ZOOM_MAP_VIEW_INDEX_STR, "Map view indexx");
-}
-
-void CZoomDlg::OnBnClickedRadioZoomMapView1()
-{
-	Update();
-}
-
-void CZoomDlg::OnBnClickedRadioZoomMapView2()
-{
-	Update();
-}
-
-void CZoomDlg::OnBnClickedRadioZoomMapView3()
-{
-	Update();
+	CFmiWin32Helpers::SetDialogItemText(this, IDC_STATIC_ZOOM_MAP_VIEW_INDEX_STR, "Used map view indexes");
+	CFmiWin32Helpers::SetDialogItemText(this, IDC_CHECK_ALLOW_MIXED_MAP_AREA_ZOOMING, "Allow mixed map area zoom");
 }
 
 void CZoomDlg::ActivateMapViewDescTop(int theDescTopIndex)
 {
-	itsSelectedMapViewDescTopIndex = theDescTopIndex;
+	if(theDescTopIndex == 0)
+		fMapView1Enabled = TRUE;
+	if(theDescTopIndex == 1)
+		fMapView2Enabled = TRUE;
+	if(theDescTopIndex == 2)
+		fMapView3Enabled = TRUE;
+
 	UpdateData(FALSE);
 	Update();
 	SetActiveWindow();
+}
+
+void CZoomDlg::OnBnClickedCheckMapView1Enabled()
+{
+	UpdateData(TRUE);
+	Update();
+}
+
+void CZoomDlg::OnBnClickedCheckMapView2Enabled()
+{
+	UpdateData(TRUE);
+	Update();
+}
+
+void CZoomDlg::OnBnClickedCheckMapView3Enabled()
+{
+	UpdateData(TRUE);
+	Update();
+}
+
+
+void CZoomDlg::OnBnClickedCheckAllowMixedMapAreaZooming()
+{
+	UpdateData(TRUE);
 }
