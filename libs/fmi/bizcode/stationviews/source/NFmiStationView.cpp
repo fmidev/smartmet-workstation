19486c75e55c02fa702ee6db61c017b07944fb61
Marko Pietarinen <mpietarin@gmail.com>
2024-02-05 15:15:11 +0200

    Fixed NFmiStationView::CalcUsedSpaceOutFactors(int theSpaceOutFactor) method to make sure that returned space-out factors are minimum of value 1. Several CrashRpt reports suggest that in NFmiStationView::SbdCollectSpaceOutSymbolDrawData there might been value of 0 in either x/y dimension of this factor. Program has crashed when modulus "currentLine % spaceOutFactorY == 0" is calculated. Hopefully this will fix this issue.

--- libs/fmi/bizcode/stationviews/source/NFmiStationView.cpp
+++ libs/fmi/bizcode/stationviews/source/NFmiStationView.cpp
@@ -644,7 +644,13 @@ static boost::shared_ptr<NFmiFastQueryInfo> GetUsedSpaceOutCalculationInfo(boost
 	return normalInfo;
 }
 
+#ifdef max
+#undef max
+#endif
+
+
 // OLETUS!! itsInfo on grid-dataa.
+// HUOM! Pitää varmistaa että tulos on minimissään 1.
 NFmiPoint NFmiStationView::CalcUsedSpaceOutFactors(int theSpaceOutFactor)
 {
 	if(theSpaceOutFactor == 0)
@@ -680,17 +686,31 @@ NFmiPoint NFmiStationView::CalcUsedSpaceOutFactors(int theSpaceOutFactor)
         double xFactor = fontFactor.X() * fontXSize * textLength / distX;
         double yFactor = fontFactor.Y() * fontYSize / distY;
 		DoSymboldrawDensityAdjustments(xFactor, yFactor);
-
+		double finalFactorX = 1;
+		double finalFactorY = 1;
         // 6. case theSpaceOutFactor 1 -> ceil, 2 -> ceil + 1
-        if(theSpaceOutFactor == 1)
-            return NFmiPoint(::ceil(xFactor), ::ceil(yFactor));
+		if(theSpaceOutFactor == 1)
+		{
+			finalFactorX = std::ceil(xFactor);
+			finalFactorY = std::ceil(yFactor);
+		}
         else //if(theSpaceOutFactor == 2)
         {
             if(xFactor < 0.5 && yFactor < 0.5) // jos ollaan zoomattu jo tarpeeksi lähellä, näytetään kaikki pisteet
-                return NFmiPoint(::ceil(xFactor), ::ceil(yFactor));
+			{
+				finalFactorX = std::ceil(xFactor);
+				finalFactorY = std::ceil(yFactor);
+			}
             else
-                return NFmiPoint(::ceil(xFactor) + 1, ::ceil(yFactor) + 1);
+			{
+				finalFactorX = std::ceil(xFactor) + 1;
+				finalFactorY = std::ceil(yFactor) + 1;
+			}
         }
+
+		finalFactorX = std::max(finalFactorX, 1.);
+		finalFactorY = std::max(finalFactorY, 1.);
+		return NFmiPoint(finalFactorX, finalFactorY);
     }
 }
 
