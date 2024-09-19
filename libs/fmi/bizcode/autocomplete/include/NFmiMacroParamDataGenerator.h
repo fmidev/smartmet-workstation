c956615dd7685b00cf820046b7b0925d241fb621
Marko Pietarinen <mpietarin@gmail.com>
2024-09-19 16:51:26 +0300

    1. Made new NFmiMacroParamDataInfo class that is used to store different macroParam generation data. 2. NFmiMacroParamDataGenerator can now generate new querydata from given NFmiMacroParamDataInfo object - There are several checks on given data - Basic structure is there now

--- libs/fmi/bizcode/autocomplete/include/NFmiMacroParamDataGenerator.h
+++ libs/fmi/bizcode/autocomplete/include/NFmiMacroParamDataGenerator.h
@@ -2,16 +2,73 @@
 #include "NFmiCachedRegistryValue.h"
 #include <string>
 
+class NFmiQueryData;
+class NFmiFastQueryInfo;
+class NFmiInfoOrganizer;
+
+// Tämä on yksi erillinen MacroParam dataan liittyvien datojen kokoelma.
+// Eli systeemissä voi olla useita erilaisia MacroParam datojen reseptejä,
+// ja nee laitetaan tälläisiin otuksiin.
+// Inputit tarkistetään tämän avulla.
+class NFmiMacroParamDataInfo
+{
+    // Pohjadata valitaan antamalla parametri+tuottaja+mahdollinen-level tieto
+    // Esim1: T_ec eli Ecmwf:n pinta data, missä lämpötilaparametri
+    // Esim2: par4_prod240 eli sama Ec-pinta data, mutta annettuna par+prod id:en avulla
+    // Esim2: par4_prod240_lev120 eli ecmwf hybrid data, missä hybrid-leveli 120
+    std::string mBaseDataParamProducerLevelString; // PAKOLLINEN
+    // Talletettavan datan tuottajan id,name
+    // Esim: "1278,ProducerName"
+    std::string mUsedProducerString; // PAKOLLINEN
+    // Polku (suhteellinen/absoluuttinen) siihen smarttool tiedostoon, mitä käytetään 
+    // generoimaan lopullinen data ja sen kaikki parametrit.
+    // Tähän pyritään saamaan aina suhteellinen polku, jos se osoittaa smarttool kansioon Dropboxissa.
+    std::string mDataGeneratingSmarttoolPathString; // PAKOLLINEN
+    // Pohjadatasta otetaan muut dimensiot (hplace/vplace/time), mutta käyttäjä määrää
+    // mitkä parametrit dataan generoidaan. Tässä on pilkuilla eroteltuna id1,name1,id2,name2,...
+    // Esim. 2501,MyParam1,2502,MyParam2,2503,MyParam3,2504,MyParam4
+    std::string mUsedParameterListString; // PAKOLLINEN
+    // Mihin hakemistoon ja millä nimellä data talletetaan, kun se generoidaan.
+    // Tämä voi olla myös tyhjä, jolloin data otetaan vain lokaali käyttöön Smartmetin sisäiseen muistiin.
+    // Esim. D:\data\*_mydata.sqd
+    // Tässä * kohtaan laitetaan lokaaliajan seinäkelloaika sekunteja myöten:
+    // Esim. D:\data\20240912183423_mydata.sqd
+    std::string mDataStorageFileFilter; // PAKOLLINEN
+
+    // Initialisoinnista raportoiva teksti
+    std::string itsInitializeLogStr;
+    bool fDataChecked = false;
+public:
+    NFmiMacroParamDataInfo();
+    NFmiMacroParamDataInfo(const std::string &baseDataParamProducerLevelString, const std::string &usedProducerString, const std::string &dataGeneratingSmarttoolPathString, const std::string &usedParameterListString, const std::string &dataStorageFileFilter);
+
+    bool CheckData();
+    bool DataChecked() const { return fDataChecked; }
+    const std::string GetInitializeLogStr() const { return itsInitializeLogStr; }
+
+    const std::string& BaseDataParamProducerLevelString() const { return mBaseDataParamProducerLevelString; }
+    const std::string& UsedProducerString() const { return mUsedProducerString; }
+    const std::string& DataGeneratingSmarttoolPathString() const { return mDataGeneratingSmarttoolPathString; }
+    const std::string& UsedParameterListString() const { return mUsedParameterListString; }
+    const std::string& DataStorageFileFilter() const { return mDataStorageFileFilter; }
+
+    static void CheckDataStorageFileFilter(const std::string& dataStorageFileFilter);
+    static std::string MakeDataStorageFilePath(const std::string& dataStorageFileFilter);
+};
+
 // Tällä talletetaan paljon MacroParamDataGenerator dialogin juttuja 
 // Windows rekisteriin pysyvään muistiin jossa käytetään
 // boost::shared_ptr<CachedRegString> datarakennetta.
+// Luokan tarkoitus on luoda uusia MacroParam datoja halutuilla skripteillä ja asetuksilla.
 class NFmiMacroParamDataGenerator
 {
     // General MacroParam Data Generator dialog options
     // Perus smartmet polku Windows rekistereissä (tähän tulee SmartMetin konfiguraatio kohtainen polku)
     std::string mBaseRegistryPath; 
-    // Juuri smarttools hakemisto, jossa on \ merkki lopussa
-    std::string mRootSmarttoolsDirectory;
+    // Juuri smarttool hakemisto, jossa on \ merkki lopussa
+    std::string mRootSmarttoolDirectory;
+
+    // HUOM! mDialog -alkuiset data memberit pitävät sisällään vastaavan dialogin kenttien sisällöt.
 
     // Pohjadata valitaan antamalla parametri+tuottaja+mahdollinen-level tieto
     // Esim1: T_ec eli Ecmwf:n pinta data, missä lämpötilaparametri
@@ -35,11 +92,19 @@ class NFmiMacroParamDataGenerator
     // Tässä * kohtaan laitetaan lokaaliajan seinäkelloaika sekunteja myöten:
     // Esim. D:\data\20240912183423_mydata.sqd
     boost::shared_ptr<CachedRegString> mDialogDataStorageFileFilter; // PAKOLLINEN
+
+
     // Initialisoinnista raportoiva teksti
     std::string itsInitializeLogStr;
+    // Mahdollinen smarttool laskuihin tai sen alustuksiin liittyviä ongelmia laitetaan tähän talteen
+    std::string itsSmarttoolCalculationLogStr;
+    // Tiedosto josta luettiin smarttool
+    std::string mUsedAbsoluteSmarttoolPath;
 public:
     NFmiMacroParamDataGenerator();
-    bool Init(const std::string& theBaseRegistryPath, const std::string& rootSmarttoolsDirectory);
+    bool Init(const std::string& theBaseRegistryPath, const std::string& rootSmarttoolDirectory);
+
+    bool GenerateMacroParamData();
 
     std::string DialogBaseDataParamProducerLevelString() const;
     void DialogBaseDataParamProducerLevelString(const std::string& newValue);
@@ -54,7 +119,13 @@ public:
     std::string DialogDataStorageFileFilter() const;
     void DialogDataStorageFileFilter(const std::string& newValue);
 
-    const std::string GetInitializeLogStr() const { return itsInitializeLogStr; }
+    const std::string& GetInitializeLogStr() const { return itsInitializeLogStr; }
+    const std::string& GetSmarttoolCalculationLogStr() const { return itsSmarttoolCalculationLogStr; }
 
 private:
+    NFmiMacroParamDataInfo MakeDataInfo() const;
+    bool CalculateDataWithSmartTool(boost::shared_ptr<NFmiFastQueryInfo>& wantedMacroParamInfoPtr, NFmiInfoOrganizer* infoOrganizer, const std::string& smartToolText);
+    std::string ReadSmarttoolContentFromFile(const std::string& filePath);
+    bool StoreMacroParamData(boost::shared_ptr<NFmiQueryData>& macroParamDataPtr, const std::string& dataStorageFileFilter);
+    bool GenerateMacroParamData(const NFmiMacroParamDataInfo &dataInfo);
 };
--- libs/fmi/bizcode/autocomplete/source/NFmiMacroParamDataGenerator.cpp
+++ libs/fmi/bizcode/autocomplete/source/NFmiMacroParamDataGenerator.cpp
@@ -1,5 +1,95 @@
 #include "NFmiMacroParamDataGenerator.h"
 #include "NFmiPathUtils.h"
+#include "NFmiSmartToolIntepreter.h"
+#include "NFmiSmartToolModifier.h"
+#include "NFmiExtraMacroParamData.h"
+#include "CtrlViewDocumentInterface.h"
+#include "NFmiInfoOrganizer.h"
+#include "NFmiParamDescriptor.h"
+#include "NFmiQueryData.h"
+#include "NFmiFastQueryInfo.h"
+#include "NFmiQueryDataUtil.h"
+#include "NFmiFileSystem.h"
+#include "NFmiPathUtils.h"
+#include <boost/algorithm/string.hpp>
+#include <filesystem>
+
+namespace
+{
+    bool DirectoryExists(const std::string& directoryPath)
+    {
+        return (std::filesystem::exists(directoryPath) && std::filesystem::is_directory(directoryPath));
+    }
+
+    std::string GetFileExtension(const std::string& filePath)
+    {
+        std::filesystem::path originalPath(filePath);
+        return originalPath.extension().string();
+    }
+}
+
+// ***********************************************************
+// ************** NFmiMacroParamDataInfo *********************
+// ***********************************************************
+
+NFmiMacroParamDataInfo::NFmiMacroParamDataInfo() = default;
+
+NFmiMacroParamDataInfo::NFmiMacroParamDataInfo(const std::string& baseDataParamProducerLevelString, const std::string& usedProducerString, const std::string& dataGeneratingSmarttoolPathString, const std::string& usedParameterListString, const std::string& dataStorageFileFilter)
+:mBaseDataParamProducerLevelString(baseDataParamProducerLevelString)
+,mUsedProducerString(usedProducerString)
+,mDataGeneratingSmarttoolPathString(dataGeneratingSmarttoolPathString)
+,mUsedParameterListString(usedParameterListString)
+,mDataStorageFileFilter(dataStorageFileFilter)
+{
+
+}
+
+bool NFmiMacroParamDataInfo::CheckData()
+{
+    return false;
+}
+
+std::string NFmiMacroParamDataInfo::MakeDataStorageFilePath(const std::string& dataStorageFileFilter)
+{
+    // Tehd‰‰n lokaali ajasta aikaleima, jossa mukana myˆs sekunnit
+    NFmiTime makeTime;
+    std::string timeStampStr = makeTime.ToStr(kYYYYMMDDHHMMSS);
+    auto finalFilePath = dataStorageFileFilter;
+    boost::replace_all(finalFilePath, "*", timeStampStr);
+    return finalFilePath;
+}
+
+const std::string gWantedFileExtension = ".sqd";
+
+// Talletettavan datan filefiltterin tarkistus. Heitt‰‰ poikkeuksen, jos siin‰ on jotain vikaa.
+void NFmiMacroParamDataInfo::CheckDataStorageFileFilter(const std::string& dataStorageFileFilter)
+{
+    // 1. Polku ei kelpaa, jos talletushakemistoa ei ole.
+    // T‰ll‰ tarkoitus est‰‰ datojen vahinkotalletuksia v‰‰riin paikkoihin.
+    auto directory = PathUtils::getPathSectionFromTotalFilePath(dataStorageFileFilter);
+    if(!::DirectoryExists(directory))
+    {
+        throw std::runtime_error(std::string(__FUNCTION__) + " given data output directory '" + directory + "' doesn't exists, make sure that given path for MacroParam data was correct!");
+    }
+
+    auto filename = PathUtils::getFilename(dataStorageFileFilter);
+    // 2. Tiedoston nimess‰ pit‰‰ olla '*' merkki aikaleimaa varten.
+    if(filename.find('*') == std::string::npos)
+    {
+        throw std::runtime_error(std::string(__FUNCTION__) + " given data output filename '" + filename + "' (in " + dataStorageFileFilter + ") doesn't have ' * ' character place marker for file's creation time stamp. To store MacroParam data you need to have that asterisk in filename.");
+    }
+
+    auto fileExtension = ::GetFileExtension(dataStorageFileFilter);
+    // 3. Tiedoston extension pit‰‰ olla sqd tyyppinen
+    if(!boost::iequals(fileExtension, gWantedFileExtension))
+    {
+        throw std::runtime_error(std::string(__FUNCTION__) + " given data output extension '" + fileExtension + "' (in " + dataStorageFileFilter + ") is invalid, extension must be '" + gWantedFileExtension + ".sqd' type.");
+    }
+}
+
+// ***********************************************************
+// ************ NFmiMacroParamDataGenerator ******************
+// ***********************************************************
 
 NFmiMacroParamDataGenerator::NFmiMacroParamDataGenerator() = default;
 
@@ -13,10 +103,10 @@ namespace
     }
 }
 
-bool NFmiMacroParamDataGenerator::Init(const std::string& theBaseRegistryPath, const std::string& rootSmarttoolsDirectory)
+bool NFmiMacroParamDataGenerator::Init(const std::string& theBaseRegistryPath, const std::string& rootSmarttoolDirectory)
 {
     mBaseRegistryPath = theBaseRegistryPath;
-    mRootSmarttoolsDirectory = ::FixRootPath(rootSmarttoolsDirectory);
+    mRootSmarttoolDirectory = ::FixRootPath(rootSmarttoolDirectory);
     // HKEY_CURRENT_USER -keys
     HKEY usedKey = HKEY_CURRENT_USER;
     // Beta product section
@@ -26,6 +116,208 @@ bool NFmiMacroParamDataGenerator::Init(const std::string& theBaseRegistryPath, c
     mDialogDataGeneratingSmarttoolPathString = ::CreateRegValue<CachedRegString>(mBaseRegistryPath, macroParamDataGeneratorSectionName, "\\DataGeneratingSmarttoolPath", usedKey, "smarttool.st");
     mDialogUsedParameterListString = ::CreateRegValue<CachedRegString>(mBaseRegistryPath, macroParamDataGeneratorSectionName, "\\UsedParameterListString", usedKey, "6201, Param1, 6202, Param2");
     mDialogDataStorageFileFilter = ::CreateRegValue<CachedRegString>(mBaseRegistryPath, macroParamDataGeneratorSectionName, "\\DataStorageFileFilter", usedKey, "C:\\data\\*_mydata.sqd");
+
+    return true;
+}
+
+static NFmiProducer MakeWantedProducer(const NFmiMacroParamDataInfo& dataInfo)
+{
+    std::vector<std::string> parts;
+    boost::split(parts, dataInfo.UsedProducerString(), boost::is_any_of(","));
+    if(parts.size() < 2)
+    {
+        throw std::runtime_error(std::string(__FUNCTION__) + " had too few producer parts (< 2, should be like \"123,producerName\")");
+    }
+
+    unsigned long producerId = std::stoul(parts[0]);
+    return NFmiProducer(producerId, parts[1]);
+}
+
+static NFmiParam MakeWantedParam(const std::string &part1, const std::string& part2)
+{
+    unsigned long paramId = std::stoul(part1);
+    return NFmiParam(paramId, part2);
+}
+
+static NFmiParamDescriptor MakeWantedParamDescriptor(const NFmiMacroParamDataInfo& dataInfo)
+{
+    auto producer = ::MakeWantedProducer(dataInfo);
+    std::vector<std::string> parts;
+    boost::split(parts, dataInfo.UsedParameterListString(), boost::is_any_of(","));
+    if(parts.size() < 2)
+    {
+        throw std::runtime_error(std::string(__FUNCTION__) + " had too few parameter (list) parts (< 2, should be like \"123,paramName1[,124,paramName2,...]\")");
+    }
+    
+    NFmiParamBag wantedParams;
+    size_t totalPartCounter = 0;
+    for( ; totalPartCounter < parts.size() - 1; )
+    {
+        auto wantedParam = ::MakeWantedParam(parts[totalPartCounter], parts[totalPartCounter + 1]);
+        wantedParams.Add(NFmiDataIdent(wantedParam, producer));
+        totalPartCounter += 2;
+    }
+    return NFmiParamDescriptor(wantedParams);
+}
+
+// Oletus: foundInfo on jo tarkistettu ettei se ole nullptr
+static boost::shared_ptr<NFmiQueryData> MakeWantedEmptyData(boost::shared_ptr<NFmiFastQueryInfo>& foundInfo, const NFmiParamDescriptor& wantedParamDescriptor)
+{
+    NFmiQueryInfo metaInfo(wantedParamDescriptor, foundInfo->TimeDescriptor(), foundInfo->HPlaceDescriptor(), foundInfo->VPlaceDescriptor());
+    return boost::shared_ptr<NFmiQueryData>(NFmiQueryDataUtil::CreateEmptyData(metaInfo));
+}
+
+static NFmiInfoOrganizer* GetInfoOrganizer()
+{
+    return CtrlViewDocumentInterface::GetCtrlViewDocumentInterfaceImplementation()->InfoOrganizer();
+}
+
+std::string NFmiMacroParamDataGenerator::ReadSmarttoolContentFromFile(const std::string& filePath)
+{
+    mUsedAbsoluteSmarttoolPath = MakeUsedAbsoluteSmarttoolPathString(filePath);
+    std::string fileContent;
+    if(NFmiFileSystem::ReadFile2String(mUsedAbsoluteSmarttoolPath, fileContent))
+    {
+        return fileContent;
+    }
+
+    throw std::runtime_error(std::string(__FUNCTION__) + " unable to read smarttool file content from given file: " + mUsedAbsoluteSmarttoolPath);
+}
+
+
+bool NFmiMacroParamDataGenerator::GenerateMacroParamData()
+{
+    auto dataInfo = MakeDataInfo();
+    return GenerateMacroParamData(dataInfo);
+}
+
+bool NFmiMacroParamDataGenerator::GenerateMacroParamData(const NFmiMacroParamDataInfo& dataInfo)
+{
+    try
+    {
+        auto variableDataType = NFmiSmartToolIntepreter::CheckForVariableDataType(dataInfo.BaseDataParamProducerLevelString());
+        if(!variableDataType.first)
+        {
+            // Raportoi huonosta syˆtteest‰
+            return false;
+        }
+
+        auto* infoOrganizer = ::GetInfoOrganizer();
+        if(!infoOrganizer)
+        {
+            // Raportoi ongelmasta infoOrganizer:in suhteen
+            return false;
+        }
+
+        // NFmiExtraMacroParamData::FindWantedInfo metodin 3. parametri (false) tarkoittaa ett‰ pohjadataksi 
+        // ei kelpuuteta asemadataa, vain hiladata kelpaa.
+        auto wantedInfo = NFmiExtraMacroParamData::FindWantedInfo(*infoOrganizer, variableDataType.second, false);
+        if(!wantedInfo.foundInfo_)
+        {
+            // Raportoi ongelmasta kun dataa ei lˆytynyt
+            return false;
+        }
+
+        auto wantedParamDescriptor = ::MakeWantedParamDescriptor(dataInfo);
+        auto wantedMacroParamDataPtr = ::MakeWantedEmptyData(wantedInfo.foundInfo_, wantedParamDescriptor);
+        boost::shared_ptr<NFmiFastQueryInfo> wantedMacroParamInfoPtr(new NFmiFastQueryInfo(wantedMacroParamDataPtr.get()));
+        if(!wantedMacroParamInfoPtr)
+        {
+            // Raportoi ongelmasta kun fastInfo on tyhj‰‰
+            return false;
+        }
+
+        auto smarttoolContent = ReadSmarttoolContentFromFile(dataInfo.DataGeneratingSmarttoolPathString());
+        if(smarttoolContent.empty())
+        {
+            // Raportoi smarttool tiedoston luku ongelmista
+            return false;
+        }
+
+        if(CalculateDataWithSmartTool(wantedMacroParamInfoPtr, infoOrganizer, smarttoolContent))
+        {
+            return StoreMacroParamData(wantedMacroParamDataPtr, dataInfo.DataStorageFileFilter());
+        }
+    }
+    catch(std::exception& )
+    {
+        // Raportoi ongelmasta
+    }
+
+    return false;
+}
+
+// Oletus: macroParamDataPtr pit‰‰ sis‰ll‰‰n talletettavan datan.
+bool NFmiMacroParamDataGenerator::StoreMacroParamData(boost::shared_ptr<NFmiQueryData>& macroParamDataPtr, const std::string& dataStorageFileFilter)
+{
+    // CheckDataStorageFileFilter funktio heitt‰‰ poikkeuksen virheilmoituksineen, jos filefilterissa jotain vikaa.
+    NFmiMacroParamDataInfo::CheckDataStorageFileFilter(dataStorageFileFilter);
+
+    auto finalDataFilePath = NFmiMacroParamDataInfo::MakeDataStorageFilePath(dataStorageFileFilter);
+    // NFmiQueryData::Write heitt‰‰ poikkeuksia, jos tulee ongelmia.
+    macroParamDataPtr->Write(finalDataFilePath);
+    return true;
+}
+
+// Oletus: wantedMacroParamInfoPtr on tarkistettu jo ulkopuolella ett‰ ei ole nullptr
+bool NFmiMacroParamDataGenerator::CalculateDataWithSmartTool(boost::shared_ptr<NFmiFastQueryInfo>& wantedMacroParamInfoPtr, NFmiInfoOrganizer* infoOrganizer, const std::string& smartToolContent)
+{
+    itsSmarttoolCalculationLogStr.clear();
+
+    NFmiSmartToolModifier smartToolModifier(infoOrganizer);
+    try // ensin tulkitaan macro
+    {
+        smartToolModifier.SetGriddingHelper(CtrlViewDocumentInterface::GetCtrlViewDocumentInterfaceImplementation()->GetGriddingHelper());
+        smartToolModifier.IncludeDirectory(mRootSmarttoolDirectory);
+        smartToolModifier.InitSmartTool(smartToolContent);
+        smartToolModifier.SetFixedEditedData(wantedMacroParamInfoPtr);
+    }
+    catch(std::exception& e)
+    {
+        itsSmarttoolCalculationLogStr = e.what();
+        std::string errorString = e.what();
+        errorString += ", in macro '";
+        errorString += mUsedAbsoluteSmarttoolPath;
+        errorString += "'";
+        throw std::runtime_error(errorString);
+    }
+
+    // Jos kyseess‰ on ns. macroParam eli skriptiss‰ on lausekkeit‰ jossa sijoitetaan 
+    // RETURN muuttujaan, t‰llˆin ei haluta ajaa makro muutoksia, koska tarkoitus 
+    // on vain katsella macroParam:ia karttan‰ytˆll‰.
+    // T‰nne asti tullaan ett‰ saadaan virhe ilmoitus tulkinta vaiheessa talteen.
+    // Mutta suoritus vaiheen virheet menev‰t t‰ll‰ hetkell‰ vain loki tiedostoon.
+    if(smartToolModifier.IsInterpretedSkriptMacroParam())
+    {
+        throw std::runtime_error(std::string("Used script '") + mUsedAbsoluteSmarttoolPath + "' was macroParam type. You have to use smarttool scripts when generating MacroParam data, not macroParam scripts, even though data's name might suggest otherwise");
+    }
+
+    // suoritetaan macro sitten
+    try
+    {
+        NFmiThreadCallBacks* threadCallBacks = nullptr;
+        auto usedTimeDescriptor(wantedMacroParamInfoPtr->TimeDescriptor());
+        smartToolModifier.ModifyData_ver2(&usedTimeDescriptor, false, false, threadCallBacks);
+
+        if(!smartToolModifier.LastExceptionMessageFromThreads().empty())
+        {
+            itsSmarttoolCalculationLogStr = smartToolModifier.LastExceptionMessageFromThreads();
+            std::string errorString = smartToolModifier.LastExceptionMessageFromThreads();
+            errorString += ", in macro '";
+            errorString += mUsedAbsoluteSmarttoolPath;
+            errorString += "'";
+            throw std::runtime_error(errorString);
+        }
+    }
+    catch(std::exception& e)
+    {
+        itsSmarttoolCalculationLogStr = e.what();
+        std::string errorString = e.what();
+        errorString += ", in macro '";
+        errorString += mUsedAbsoluteSmarttoolPath;
+        errorString += "'";
+        throw std::runtime_error(errorString);
+    }
 
     return true;
 }
@@ -51,18 +343,18 @@ void NFmiMacroParamDataGenerator::DialogDataGeneratingSmarttoolPathString(const 
     *mDialogDataGeneratingSmarttoolPathString = newValue;
 }
 
-// Jos annettu polku on suhteellinen, lis‰t‰‰n siihen smarttools juuri hakemisto.
+// Jos annettu polku on suhteellinen, lis‰t‰‰n siihen smarttool juuri hakemisto.
 // Korjataan polku kaikin puolin oikeaan formaattiin (erottimet ja mahd. .. tai . jutut).
 std::string NFmiMacroParamDataGenerator::MakeUsedAbsoluteSmarttoolPathString(const std::string& smarttoolPath) const
 {
-    return PathUtils::getAbsoluteFilePath(smarttoolPath, mRootSmarttoolsDirectory);
+    return PathUtils::getAbsoluteFilePath(smarttoolPath, mRootSmarttoolDirectory);
 }
 
-// Jos annettu polku on absoluuttinen, poistetaan siit‰ smarttools juuri hakemisto osio alusta.
+// Jos annettu polku on absoluuttinen, poistetaan siit‰ smarttool juuri hakemisto osio alusta.
 // Korjataan polku kaikin puolin oikeaan formaattiin (erottimet ja mahd. .. tai . jutut).
 std::string NFmiMacroParamDataGenerator::MakeCleanedSmarttoolPathString(const std::string& smarttoolPath) const
 {
-    auto tmpPath = PathUtils::getRelativePathIfPossible(smarttoolPath, mRootSmarttoolsDirectory);
+    auto tmpPath = PathUtils::getRelativePathIfPossible(smarttoolPath, mRootSmarttoolDirectory);
     return PathUtils::simplifyWindowsPath(tmpPath);
 }
 
@@ -94,4 +386,9 @@ std::string NFmiMacroParamDataGenerator::DialogDataStorageFileFilter() const
 void NFmiMacroParamDataGenerator::DialogDataStorageFileFilter(const std::string& newValue)
 {
     *mDialogDataStorageFileFilter = newValue;
+}
+
+NFmiMacroParamDataInfo NFmiMacroParamDataGenerator::MakeDataInfo() const
+{
+    return NFmiMacroParamDataInfo(DialogBaseDataParamProducerLevelString(), DialogUsedProducerString(), DialogDataGeneratingSmarttoolPathString(), DialogUsedParameterListString(), DialogDataStorageFileFilter());
 }
