3b20584d43d8c03e7f49f814522d88bc102723a3
Marko Pietarinen <mpietarin@gmail.com>
2025-09-15 18:17:36 +0300

    Changed to beta mode, with warning text: https support test

--- libs/fmi/bizcode/generaldoc/source/NFmiBasicSmartMetConfigurations.cpp
+++ libs/fmi/bizcode/generaldoc/source/NFmiBasicSmartMetConfigurations.cpp
@@ -522,11 +522,11 @@ void NFmiBasicSmartMetConfigurations::MakeSplashScreenTextDataVector(const NFmiT
     copyrightStringU_ += _TEXT(" Finnish Meteorological Institute");
     itsSplashScreenTextDataVector.push_back(DrawStringData(copyrightStringU_, _TEXT("Arial"), 18, RGB(0, 0, 0), CPoint(22, 325), true));
 
-    bool betaVersion = false;
+    bool betaVersion = true;
     if(betaVersion)
         itsSplashScreenTextDataVector.push_back(DrawStringData(_TEXT("Beta"), _TEXT("Arial"), 25, RGB(255, 0, 0), CPoint(122, 267), true));
 
-    CString possibleWarningTextU_;// = _TEXT("Mahdollisia ongelmia johtuen kehittimen (VS 2019) päivityksestä");
+    CString possibleWarningTextU_ = _TEXT("https support test");
     if(possibleWarningTextU_.GetLength())
         itsSplashScreenTextDataVector.push_back(DrawStringData(possibleWarningTextU_, _TEXT("Arial"), 25, RGB(255, 0, 0), CPoint(15, 370), true));
 
